5d90b9192a5e8d4691581d8151cca96f64e3b1c6
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-08-09 14:11:18 +0200

    Update Main.java

--- src/main/java/one/empty3/feature/Main.java
+++ src/main/java/one/empty3/feature/Main.java
@@ -12,13 +12,14 @@ import java.nio.file.*;
 
 public class Main {
     public static void makeGoodOutput(File original, File folderOutput, PrintWriter out) {
+        try{
 Path source = FileSystems.getDefault().getPath(original.getAbsolutePath()) ;
      Path newdir = FileSystems.getDefault().getPath(folderOutput.getAbsolutePath());
      Files.copy(source, newdir.resolve(source.getFileName()));
       //  out.println("<a href=\""+original.getName()+"\">orginal</a>\n");
        // out.println("<a href=\""+folderOutput.getPath()+"\">computed result image folder</a>\n");
-    }
-    
+    }catch(Exceptoon ex){ex.printStackTrace();}
+    } 
     public static void main(String[] args) {
         Arrays.stream(ImageIO.getWriterFormatNames()).forEach(s1 ->
                 System.out.println("Format name : \"" + s1 + "\""));
