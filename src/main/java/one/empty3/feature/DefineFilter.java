5f18d0e3ee280de2195a91b257eaba0189e5c660
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-08-09 20:41:54 +0200

    Create DefineFilter.java

---
+++ src/main/java/one/empty3/feature/DefineFilter.java
@@ -0,0 +1,34 @@
+package one.empty3.feature;
+public class DefineFilter extends FilterPixM {
+
+    
+    public DefineFilter(double [][] matrix, double divider) {
+        super(matrix.length, matrix[0].length);
+        fill(matrix);
+    }
+
+    @Override
+    public double filter(double x, double y) {
+    
+        return 1.0 * get((integer)x-columns/2, (integer)y-lines/2);
+             
+;
+    }
+
+    /***
+     * Gaussian filter Matrix
+     * @param matrix 2d double
+     */
+   
+/*
+    public void fill() {
+    for(int comp = 0 ; comp<getCompCount(); comp++) {
+        setCompNo(comp);
+        for (int i = 0; i < columns; i++)
+            for (int j = 0; j < lines; j++) {
+                set(i, j, matrix[j][x]
+                );
+            }
+       }
+    }
+}
