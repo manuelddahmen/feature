f0d809692c56a9830c63d63dcb931f863cb6eff1
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-08-10 18:30:30 +0200

    Create SobelDerivates.java

---
+++ src/main/java/one/empty3/feature/SobelDerivates.java
@@ -0,0 +1,18 @@
+package one.empty3.feature;
+
+public SobelDerivative extends PixM{
+     public SobelDerivate(int c, int l) {
+          super(c, l);
+     }
+     public SobelDerivate(BufferedImage image) {
+          super(image);
+     }
+     public double x(int i, int j) {
+     
+     }
+   public double y(int i, int j){
+   }
+   public theta (int i, int j) {
+   
+   }
+}
