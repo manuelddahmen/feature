28dd211cc1ff15b6dea21ff24b09bd723ef11005
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-08-10 20:09:16 +0200

    Update SobelDerivates.java

--- src/main/java/one/empty3/feature/SobelDerivates.java
+++ src/main/java/one/empty3/feature/SobelDerivates.java
@@ -4,18 +4,25 @@ sobel. 3×3 ou plus. 1*2+1
 |p1 -p2| (/ n/n)?
 */
 public SobelDerivative extends PixM{
+     int[][] sobelX = new int[] {-1,-2,-1,0,0,0
+                                ,1,2,1};
+     int[][] sobelY = new int[] {-1,0,-1,
+                                 -2,0,2
+                                ,1,0,1};
      public SobelDerivate(int c, int l) {
+          
           super(c, l);
      }
      public SobelDerivate(BufferedImage image) {
           super(image);
      }
      public double x(int i, int j) {
-     
+           return sobelX[i*lines + j];
      }
    public double y(int i, int j){
+        return sobelY[i*lines + j];
    }
-   public theta (int i, int j) {
+   public void theta (int i, int j) {
    
    }
 }
