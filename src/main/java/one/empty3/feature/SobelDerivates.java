d3e72ebeaccfb597adb1b0c9d165dfb2e0d3b492
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-08-10 18:34:31 +0200

    Update SobelDerivates.java

--- src/main/java/one/empty3/feature/SobelDerivates.java
+++ src/main/java/one/empty3/feature/SobelDerivates.java
@@ -1,5 +1,8 @@
 package one.empty3.feature;
-
+/***
+sobel. 3×3 ou plus. 1*2+1
+|p1 -p2| (/ n/n)?
+*/
 public SobelDerivative extends PixM{
      public SobelDerivate(int c, int l) {
           super(c, l);
