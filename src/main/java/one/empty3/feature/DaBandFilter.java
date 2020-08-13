18df7e292a2297facd7bb335ad173b9c65661d97
Dahmen Manuel Daniel (Second Address) <manuel.dahmen78@gmail.com>
2020-08-13 16:35:44 +0200

    Filters

---
+++ src/main/java/one/empty3/feature/DaBandFilter.java
@@ -0,0 +1,20 @@
+package one.empty3.feature;
+
+public class DaBandFilter {
+    // 1. amplitude and scale featyre
+    // Gauss
+    // 2.1 pattern based amplification filter
+    // circle : (x, y, [xc, yc,] r) (radial)
+    // mutil circle tries
+    // 2.2 linear : (x,y, xl, yl, d) (linear)
+    // other shapes
+    // 3. local amplification : { (xi, yi) }
+    // kind of feature
+    // planes of colors or magnitude
+    // gradient == 0.0 ~+ 0.1
+    // surface { (xi, yi) }
+
+    // "Donc t'aurais un feature en nombres et des reproductions dans plusieurs images.*
+
+
+}
