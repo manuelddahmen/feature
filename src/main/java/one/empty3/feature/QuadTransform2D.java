84b9135c9447cebe255f40d5e6417e4eb8c343ec
Dahmen Manuel Daniel (Second Address) <manuel.dahmen78@gmail.com>
2020-08-15 11:11:47 +0200

    Morph basic function

--- src/main/java/one/empty3/feature/QuadTransform2D.java
+++ src/main/java/one/empty3/feature/QuadTransform2D.java
@@ -1,27 +1,29 @@
 package one.empty3.feature;
 
+import one.empty3.library.Point3D;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class QuadTransform2D {
     private BufferedImage i1, i2;
-    private Point [] p1;
-    private Point [] p2;
+    private Point3D[] p1;
+    private Point3D [] p2;
 
     public class Inter {
-        Point p;
+        Point3D p;
         Color c;
 
-        public Inter(Point p, Color c) {
+        public Inter(Point3D p, Color c) {
             this.p = p;
             this.c = c;
         }
 
-        public Point getP() {
+        public Point3D getP() {
             return p;
         }
 
-        public void setP(Point p) {
+        public void setP(Point3D p) {
             this.p = p;
         }
 
@@ -34,16 +36,33 @@ public class QuadTransform2D {
         }
     }
 
-    public QuadTransform2D(BufferedImage i1, BufferedImage i2, Point[] p1, Point[] p2) {
+    public QuadTransform2D(BufferedImage i1, BufferedImage i2, Point3D[] p1, Point3D[] p2) {
         this.i1 = i1;
         this.i2 = i2;
         this.p1 = p1;
         this.p2 = p2;
     }
+    private Point3D [] quadTemp(double t) {
+        Point3D[] pTemp = new Point3D[4];
+        for (int i = 0; i < p1.length; i++) {
+            pTemp[i] = p1[i].plus(p2[i].moins(p1[i]).mult(t));
+        }
+        return pTemp;
+    }
 
+    public Point3D pT(Point3D[] pTemp, double p1243, double p1423){
+        Point3D plus0 = pTemp[0].plus(pTemp[1].moins(pTemp[0]).mult(p1243));
+        Point3D plus1 = pTemp[3].plus(pTemp[2].moins(pTemp[4]).mult(p1243));
+        return plus0.plus((plus1.moins(plus0).mult(p1423)));
+    }
     public Inter inter(double p1243, double p1423, double t) {
-
-        return null;
+        // Color cp0+(cpa0-cpaT)*t ???
+        // Texture???
+        Point3D[] pTemp = quadTemp(t);
+        Point3D[] p0 = quadTemp(0.0);
+        Point3D pa0 = pT(p0, p1243, p1423);
+        Point3D pT = pT(pTemp, p1243, p1423);
+        return new Inter(pT, null);
     }
 
 
