577e1f643816be8786c6d4e478dce0b6cd130d0e
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-08-16 10:08:24 +0200

    Update VectorsImage.java

--- src/main/java/one/empty3/feature/VectorsImage.java
+++ src/main/java/one/empty3/feature/VectorsImage.java
@@ -9,7 +9,7 @@ public class VectorsImage extends M3 {
     public VectorsImage(BufferedImage image1) {
         this(image1.getWidth(), image1.getHeight(), 2, 1);
         PixM pixM = new PixM(image1);
-        M3 gradientDoubleValuesMap =pixM.applyFilter2D(new GradientFilter(image1, 2, 1));
+        M3 gradientDoubleValuesMap =pixM.applyFilter2D(new GradientFilter(image1));
         // construire les vecteurs
     }
 
