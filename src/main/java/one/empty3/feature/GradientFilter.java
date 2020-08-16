0a9a80e6890f4d33d8d59ea0ab1099b18ebc6466
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-08-16 10:04:58 +0200

    Update GradientFilter.java

--- src/main/java/one/empty3/feature/GradientFilter.java
+++ src/main/java/one/empty3/feature/GradientFilter.java
@@ -4,15 +4,18 @@ import java.awt.image.BufferedImage;
 
 public class GradientFilter extends FilterMatPixM{
 
-    public GradientFilter(BufferedImage image, int l, int c) {
-        super(image, 1, 2);
+    public GradientFilter(BufferedImage image) {
+        super(image, 1, 1);
     }
 
     public void filter(PixM image, int i, int j, int ii, int ij) {
-        if((ii==1.0 && ij==0.0) ||(ii==0.0 && ij==1.0))
-            set(i, j, ii, ij, image.get(ii, ij)-image.get(i, j));
-        if((ii==0.0 && ij==0.0))
-            set(i, j, ii, ij,  image.get(ii, ij)-image.get(i, j));
-    }
+        set (i, j, 0, 0, ( - image.get(ii-1, ij) - 
+         - image.get(ii, ij-1) - 
+         + 4*  image.get(ii, ij)
+         + image.get(ii+1, ij)
+         + image.get(ii, ij+1) 
+      ) / 4.0);
+        
+ }
 
 }
