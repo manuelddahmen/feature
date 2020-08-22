c286056b95a00a92954e65b7ccd6de38e63b5c7e
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-08-22 10:13:11 +0200

    Create FollowLines.java

---
+++ src/main/java/one/empty3/feature/FollowLines.java
@@ -0,0 +1,11 @@
+package one.empty3.feature;
+
+public class FollowLines {
+    public FollowLines(M3 traces) {
+        this.traces = traces;
+    } 
+// point00, neighbours 01 angle10, dist11
+    public M3 processPoints() {
+        M3 points = 
+    } 
+} 
