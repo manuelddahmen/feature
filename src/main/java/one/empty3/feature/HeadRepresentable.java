24fd8674493a23827f241fb881c8f6d1d08bc2e9
Dahmen Manuel Daniel (Second Address) <manuel.dahmen78@gmail.com>
2020-08-28 23:57:49 +0200

    OK2

--- src/main/java/one/empty3/feature/HeadRepresentable.java
+++ src/main/java/one/empty3/feature/HeadRepresentable.java
@@ -16,34 +16,51 @@ import java.util.function.Consumer;
 
 public class HeadRepresentable extends Representable {
     public HeadRepresentable() {
-        StringBuilder sb = new StringBuilder("scene(");
+        StringBuilder sb = new StringBuilder();
         Properties headRb = new Properties();
         try {
-            headRb.load(new BufferedReader(new InputStreamReader(new FileInputStream(new File("resources/stl/head.properties")))));
+            File properties = new File("resources/stl/head.properties");
 
-            headRb.keySet().forEach(sb::append);
+            File sceneStl = new File("resources/stl/head.stl");
+            File sceneXml = new File("resources/stl/head.xml");
+            File txt = new File("resources/stl/head.moo");
+            headRb.load(new BufferedReader(new InputStreamReader(new FileInputStream(properties))));
 
-            sb.append(")");
+            headRb.keySet().forEach(o -> {
+                sb.append(headRb.getProperty((String) o));
+                System.out.println(o+" = "+headRb.getProperty((String)o));
+            }
+
+            );
+
+            //sb.append(')');
+
+
 
             String s = sb.toString();
 
-            Scene scene = new Scene();
-
-            Scene load = new Loader().load(new File("resources/stl/head.stl"), scene);
+            new OutputStreamWriter(new FileOutputStream(txt)).write(s);
 
 
-            File sceneL = new File("resources/stl/head.moo");
-            new PrintWriter(sceneL).println(load);
-            File sceneXml = new File("resources/stl/head.xml");
-            StringBuilder stringBuilder = new StringBuilder();
-            scene.xmlRepresentation(null, stringBuilder, load);
-            new PrintWriter(sceneXml).println(stringBuilder);
-            File sceneStl = new File("resources/stl/head.stl");
-            STLExport.save(sceneStl, load, true);
-            new PrintWriter(sceneL).println(sb.toString());
+            Scene load = new Scene();
 
-            System.out.println(load);
-        } catch (VersionNonSupporteeException | ExtensionFichierIncorrecteException | IOException e) {
+            if((load = new Loader().load(txt, load))!=null) {
+
+
+                //File sceneL = new File("resources/stl/headRewrite.moo");
+                //new PrintWriter(sceneL).println(load);
+                StringBuilder stringBuilder = new StringBuilder();
+                load.xmlRepresentation(null, stringBuilder, load);
+                new PrintWriter(sceneXml).println(stringBuilder);
+                STLExport.save(sceneStl, load, true);
+                new PrintWriter(sceneStl).println(sb.toString());
+
+                System.out.println(load);
+            }
+            else
+                System.out.println("Model moo not loaded");
+
+        } catch ( IOException | ExtensionFichierIncorrecteException | VersionNonSupporteeException e) {
             e.printStackTrace();
         }
     }
