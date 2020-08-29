df53745b4bd5865af0106137ece74b2060d029dd
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-08-29 16:29:41 +0200

    Update Histogram.java

--- src/main/java/one/empty3/feature/Histogram.java
+++ src/main/java/one/empty3/feature/Histogram.java
@@ -21,6 +21,7 @@ public class Histogram {
      * @param levels 0..n exemple = level[i][x][y] = number of points of intensity ((i/n), (i+1)/n)
      */
     public Histogram(PixM image, int levels) {
+        this.diffLevel = 1.0/levels;
         this.levels = new int[levels][image.columns][image.lines];
 
     }
@@ -30,7 +31,11 @@ public class Histogram {
     public double nPoints(int x, int y, int w, int h) {
         return 0.0;
     }
-    public List<Circle> getLevel() {
+    public Circle getLevel(Circle c) {
+        // I mean. 
+        return c;
+    } 
+    public List<Circle> getPointsOfInterest() {
        List<Circle> circles 
            = new ArrayList<>() ;
 
@@ -39,6 +44,19 @@ public class Histogram {
        // X-x2 > li-li+-1
        // i(x2, y2, r2) > i(x, y, r) + leveldiffi|||
        // stop
+       for(int i=0; i<m.columns; i++) 
+           for(int j=0; j<m.lines; j++) {
+               int r = 1;
+               double diffI = 0;
+               while(r<m.columns & & diffI<diffLevel) {
+                   Circle c1 = new Circle(i, j, r);
+                   Circle c2 = new Circle (i, j, r+1);
+
+                   double diffI = Math.abs(getLevel(c1).i-getLevel(c2).i);
+                   r++;
+              } 
+              circles.add(c1);
+           }
        return circles;
     } 
 }
