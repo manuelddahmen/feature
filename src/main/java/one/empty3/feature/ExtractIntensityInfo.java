1e898360ad4f4ad17e6032e5f5817e164aa953c2
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-08 17:49:53 +0200

    Update ExtractIntensityInfo.java

--- src/main/java/one/empty3/feature/ExtractIntensityInfo.java
+++ src/main/java/one/empty3/feature/ExtractIntensityInfo.java
@@ -28,7 +28,31 @@ public class ExtractIntensityInfo {
     public static void stream(File f) throws IOException {
         BufferedImage read = ImageIO.read(f);
 
-        PixM pix = new PixM(read);
+        PixM pix = PixM.new PixM(read, 500);
+        
+        
+        
+//                    image2 = getImageFromDir(filename2);
+             //   GradientFilter gradientMask = new GradientFilter(image1.getWidth(), image1.getHeight());
+                PixM pixMOriginal = pix;
+
+    
+
+                GradientFilter gradientMask = new GradientFilter(pixMOriginal.columns, pixMOriginal.lines);
+                M3 imgForGrad = new M3( pixMOriginal,
+2, 2);
+                M3 filter = gradientMask.filter(imgForGrad);
+                PixM[][] imagesMatrix = filter.getImagesMatrix();//.normalize(0, 1);
+
+
+//                    image1 = null;
+
+                // Zero. +++Zero orientation variation.
+                Linear linear = new Linear(imagesMatrix[1][0], imagesMatrix[0][0],
+                        new PixM(pixMOriginal.columns, pixMOriginal.lines));
+                linear.op2d2d(new char[]{'*'}, new int[][]{{1, 0}}, new int[]{2});
+                PixM smoothedGrad = linear.getImages()[2];
+        
         String s = "outputFiles/Extracts/";
         new File(s).mkdirs();
      double min = 0.0;
