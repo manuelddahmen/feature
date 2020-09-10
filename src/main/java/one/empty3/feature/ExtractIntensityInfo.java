a9c357efc4f5b408f874e912fe4643905ceabc00
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-10 09:48:13 +0200

    Update ExtractIntensityInfo.java

--- src/main/java/one/empty3/feature/ExtractIntensityInfo.java
+++ src/main/java/one/empty3/feature/ExtractIntensityInfo.java
@@ -134,7 +134,7 @@ new File(dirOut)
         }
     }
 
-    public void main(String[] args) {
+    public void static main(String[] args) {
         
          /*
         if (args.length > 1 && args[0] != null) {
