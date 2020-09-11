8eccdd33301ededc5196ff6caae554e2b1c19a79
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-11 23:02:27 +0200

    Update WriteFile.java

--- src/main/java/one/empty3/feature/WriteFile.java
+++ src/main/java/one/empty3/feature/WriteFile.java
@@ -1,1 +1,14 @@
-
+package one.empty3.feature;
+import java.awt.images.*;
+public class WriteFile {
+    int no = 1;
+    String directory = "./output/";
+    public static void init() {
+      
+    }
+  
+    public static boolean writeNext(String name, BufferedImage imageJpeg) {
+        
+        no++;
+    }
+}
