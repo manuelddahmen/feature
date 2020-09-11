d34db743075ec15ff5b18e289b2734f59084ddab
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-11 23:31:01 +0200

    Update ExtractIntensityInfo.java

--- src/main/java/one/empty3/feature/ExtractIntensityInfo.java
+++ src/main/java/one/empty3/feature/ExtractIntensityInfo.java
@@ -135,7 +135,7 @@ pointsOfInterest.stream().filter(new Predicate<Histogram2.Circle>() {
                     //   + "/0test" + f.getName() + "min" + min + "+rMin"+rMin+".jpg");
                     File outputFile2 = new File(dirOut + "/1test" + f.getName() + "min" + min + "rMin2_"+rMin+".jpg");
                     WriteImage.write(out.getImage(), "jpg", "out");
-                    WriteImage.write(read, "jpg", "original);
+                    WriteImage.write(read, "jpg", "original");
                     WriteImage.write(img3, "jpg", "result2");
                     
                     //System.out.println("files written " + outputFile+" \n"+ outputFile2);
