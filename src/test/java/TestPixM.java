2680b998de9605efa5c66d49d2c0b96fcc70c360
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-12 09:14:44 +0200

    Update TestPixM.java

--- src/test/java/TestPixM.java
+++ src/test/java/TestPixM.java
@@ -1,7 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 import one.empty3.feature;
 
 public class TestPixM {
-     public void testPixM() {
+     @Test
+     public void testPixMblack() {
          PixM p = new PixM(500, 500);
          WriteFile.write(p.getImage(), "black 500x500");
      }
