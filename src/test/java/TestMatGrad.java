48c2402554497cbd6cee42aaf4bfd407e61fb937
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-12 18:19:14 +0200

    Update TestMatGrad.java

--- src/test/java/TestMatGrad.java
+++ src/test/java/TestMatGrad.java
@@ -40,7 +40,7 @@ public class TestMatGrad {
       WriteFile.writeNext(file.getName()+"/image gradient gy", imagesMatrix[1][0].normalize(0.,1.).getImage());
       WriteFile.writeNext(file.getName()+"/image gradient phase x", imagesMatrix[0][1].normalize(0.,1.).getImage());
       WriteFile.writeNext(file.getName()+"/image gradient phase y", imagesMatrix[1][1].normalize(0.,1.).getImage());
-      });
+      }
 
 }
 }
