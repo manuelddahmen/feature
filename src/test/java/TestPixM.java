44facd1e4f7468156a1f87f0001c6a4a133b58c1
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-12 11:46:52 +0200

    Update TestPixM.java

--- src/test/java/TestPixM.java
+++ src/test/java/TestPixM.java
@@ -12,4 +12,25 @@ public class TestPixM {
          WriteFile.writeNext(p.getImage(), "black 500x500 resized from 1000x1000");
          assertTrue(p.equals(p2));
     }
+     
+     @Test
+     public void testPrimaryColors() {
+         PixM p = new PixM(500, 500);
+         colorsRegion(p, 0, 0, 250, 250, new float[]{1.0, 1.0, 1.0, 1.0});
+         WriteFile.writeNext(p.getImage(), "white 500x500");
+         PixM p2 = PixM.getPixM(new PixM(1000, 1000).getImage(), 500.0);
+         colorsRegion(p, 0, 0, 250, 250, new float[]{1.0, 1.0, 1.0, 1.0});
+         WriteFile.writeNext(p.getImage(), "white 500x500 resized from 1000x1000");
+         assertTrue(p.equals(p2));
+          
+    }
+     
+     public void colorsRegion(PixM p, int x, y, w, h, float[] comps) {
+         for(int i=x; i<x-w; i++)
+              for(int j=x; j<x-w; j++)
+                   for(int c=0; c<p.getCompCount(); c++) {
+                        p.setCompNo(c);
+                        p.set(i, j, comp[c]);
+                   }
+     }
 }
