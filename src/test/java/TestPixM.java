877c0133cfb5a547d1c514cb589e52b78ffd5427
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-12 20:04:58 +0200

    Update TestPixM.java

--- src/test/java/TestPixM.java
+++ src/test/java/TestPixM.java
@@ -11,6 +11,8 @@ public class TestPixM {
          PixM p2 = PixM.getPixM(new PixM(1000, 1000).getImage(), 500.0);
          WriteFile.writeNext(p.getImage(), "black 500x500 resized from 1000x1000");
          assertTrue(p.equals(p2));
+         PixM p3 = PixM.getPixM(p.getImage(), 500.);
+         assertTrue(p.equals(p3));
     }
      
      @Test
