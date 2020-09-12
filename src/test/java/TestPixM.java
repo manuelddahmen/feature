cdd32df7971ed7e0f5e9fb43a1ea81332a8069de
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-12 20:22:15 +0200

    Update TestPixM.java

--- src/test/java/TestPixM.java
+++ src/test/java/TestPixM.java
@@ -20,8 +20,11 @@ public class TestPixM {
          PixM p = new PixM(500, 500);
          colorsRegion(p, 0, 0, 250, 250, new double[]{1.0, 1.0, 1.0});
          WriteFile.writeNext(p.normalize(0.,1.).getImage(), "white 500x500");
-         PixM p2 = PixM.getPixM(new PixM(1000, 1000).getImage(), 500.0);
-         colorsRegion(p2, 0, 0, 250, 250, new double[]{1.0, 1.0, 1.0});
+         
+         PixX p2 = new PixM(1000, 1000);
+         colorsRegion(p2, 0, 0, 500, 500, new double[]{1.0, 1.0, 1.0});
+         p2 = PixM.getPixM(p2.getImage(), 500.0);
+         
          WriteFile.writeNext(p.normalize(0.,1.).getImage(), "white 500x500 resized from 1000x1000");
          assertTrue(p.equals(p2));
           
