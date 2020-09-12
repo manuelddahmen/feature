48aa0d4899395255f7892c2974162a8f55f4b81a
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-12 23:23:35 +0200

    Update TestMatGrad.java

--- src/test/java/TestMatGrad.java
+++ src/test/java/TestMatGrad.java
@@ -40,7 +40,7 @@ public class TestMatGrad {
     new  LocalExtrema( imagesMatrix[1][0].getColumns(), 
                       imagesMatrix[1][0].getLines(),
                       3, 2);
-  M3 pext = le.filter(new M3(imagesMatrix[0][0], 1, 1));
+  M3 pext = le.filter(new M3(imagesMatrix[0][0], 1, 1)).getImagesMatrix()[0][0];
      
      
         WriteFile.writeNext(file.getName()+"/image reduite", pixMOriginal.normalize(0.,1.).getImage());
