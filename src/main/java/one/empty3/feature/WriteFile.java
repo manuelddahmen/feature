39c173e7bf42e4fba07b2f1af3048969c3e2466f
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-12 10:03:38 +0200

    Update WriteFile.java

--- src/main/java/one/empty3/feature/WriteFile.java
+++ src/main/java/one/empty3/feature/WriteFile.java
@@ -20,7 +20,7 @@ public class WriteFile {
             
             no++;
             ImageIO.write(imageJpeg, "jpg", n);
-            Logger.notice("file written: " + n.getAbsolutePath());
+            Logger.log("file written: " + n.getAbsolutePath());
             return true;
             
         } catch(Exception ex) {
