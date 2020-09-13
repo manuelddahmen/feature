145398af4040ab9d87bb5dfd940a7f41160f7479
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-13 09:10:02 +0200

    Update AfterGradientBeforeExtemum.java

--- src/main/java/one/empty3/feature/AfterGradientBeforeExtemum.java
+++ src/main/java/one/empty3/feature/AfterGradientBeforeExtemum.java
@@ -13,7 +13,8 @@ public class AfterGradientBeforeExtemum extends FilterMatPixM {
         this.angles = angleIterations;
     }
     @Override
-    public M3 filter(M3 original) {
+    public M3 filter(M3 maximumAndGradient00m01gx02gy) {
+        M3 original = maximumAndGradient00m01gx02gy;
         /* Choisir les points filtrés min (noirs //ou monochromes -- toutes ou  composantes annulés)
          * et repérer les zones par régions (r,theta)
          * Réunir les zones par valeur et proximité.
