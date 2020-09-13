60a2c1aa7aea2770a344c07d861d49e9229fd078
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-13 19:15:25 +0200

    Create HarrisToPointInterest.java

---
+++ src/main/java/one/empty3/feature/HarrisToPointInterest.java
@@ -0,0 +1,14 @@
+package one.empty3.feature;
+
+public class HarrisToPointInterest extends FilterPixM {
+     // nabla
+     // sobel x sobel y
+     // ou gxgx gxgy 
+     //    gygx gygy
+     // p ij
+     // mat Ip, Ip~
+     // trace/determinant extract
+     // m(p) at ij
+     // localextrema filter
+     // !=0 => points of interest
+}
