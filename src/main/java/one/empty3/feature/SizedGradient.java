e35eac3e5463a22f40e8375cba388d012878d171
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-14 22:24:33 +0200

    Update SizedGradient.java

--- src/main/java/one/empty3/feature/SizedGradient.java
+++ src/main/java/one/empty3/feature/SizedGradient.java
@@ -14,13 +14,26 @@ public class SizedGradient extends FilterPixM {
     private void fill() {
 
     }
-
+    public double formula(int i, int j) {
+        double d = Math.sqrt(i*i+j*j);
+        double angle =  Math.atan (j/i*1.0)
+        Point2D vec = new Point2D(
+           d*Math.cos(2*Math.PI*angle), 
+           d*Math.sin(2*Math.PI*angle)
+        );
+        
+    }
     @Override
     public double filter(double x, double y) {
        
       
         // For for
-        // ±±
+        // ±± (x*2)/(x*x+1)*k
+        for(int i=-avg; i<avg; i++)
+           for(int j=-avg; j<avg; j++) {
+              dxy += formula(i, j);
+              count++;
+           }
         return 1.0;
     }
 }
