733ae17035c2a383ed6853a31aef38fc9a0bda37
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-26 16:08:29 +0200

    Update IntuitiveRadialGradient.java

--- src/main/java/one/empty3/feature/IntuitiveRadialGradient.java
+++ src/main/java/one/empty3/feature/IntuitiveRadialGradient.java
@@ -1,15 +1,24 @@
 public class IntuitiveRadialGradient extends PixM {
       private PixM pix;
+      private int angles = 12;
       public double filter (double x, double y, 
          double rMaxPixel, rMaxDiff) {
+
           double pixel = 0.0;
           double [] pixelExtAngular
              = new double[12] ;
-          for(int i=x-rMaxPixel; i<x+rMaxPixel; i++)
-              for(int j=x-rMaxPixel; j<x+rMaxPixel; j++)
-                  sum+= get(i, j) ;
-          arc (int x, int y, double r1, double r2, 
-             double a1, double a2) ;
+          double vp =
+              arc(x, y, 0, rMaxPixel,
+               0., 2*Math.PI) ;
+          double vfarApprox = 400000;
+          double angle =-1;
+          for(int i=0; i<angles; i++) {
+            double v =  arc (x, y, rMaxPixel, rMaxDiff, 
+              2*Math.PI*i/angles) ;
+            if(Math.abs(v-vp) <vFarApprox)
+               vfarApprox = v;
+               angle = 2*Math.PI*(i+0.5)/angles;
+          }
       } 
     public Point2D pcircle(int x, int y, 
               double angle, double r) {
@@ -20,12 +29,15 @@ public class IntuitiveRadialGradient extends PixM {
     } 
     public double arc (int x, int y, double r1, double r2, 
               double a1, double a2) {
-         double eval = 0.0;
+         double eval = 0.0; int count=0; double dist=0;
         for(int i=x-r2; i<x+r2; i++)
               for(int j=x-r2; j<x+r2; j++)
                   if((eval=Math.sqrt((x-i)*(x-i)
-   +(y-j)* (y-j) ))<=r2 & & eval> r1) 
-                  sum+= get(i, j) ;
+   +(y-j)* (y-j) ))<=r2 & & eval>= r1) {
+                  sum+= get(i, j) ;//*gauss? Derivate? 
+                  count ++;
+                  dist += eval;
+         return sum/count;
      } 
       public double avgPixXY(int x, int y) {
           
