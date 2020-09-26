2430483602ef0d79e227e9ff617be55ba2668506
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-26 20:36:36 +0200

    Update IntuitiveRadialGradient.java

--- src/main/java/one/empty3/feature/IntuitiveRadialGradient.java
+++ src/main/java/one/empty3/feature/IntuitiveRadialGradient.java
@@ -7,6 +7,7 @@ public class IntuitiveRadialGradient extends FilterPixM {
       private int angles = 12;
 double rMaxPixel=2.0, rMaxDiff = 5.0;
       public IntuitiveRadialGradient(PixM image) {
+      super(image.columns, image.lines)
 this.pix = image;
       } 
 
