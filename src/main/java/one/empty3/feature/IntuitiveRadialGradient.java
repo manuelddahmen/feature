895a936b5116990bec3e20dc483f351f38b87df3
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-26 15:18:19 +0200

    Create IntuitiveRadialGradient.java

---
+++ src/main/java/one/empty3/feature/IntuitiveRadialGradient.java
@@ -0,0 +1,9 @@
+public class IntuitiveRadialGradient {
+      public double filter (double x, double y, 
+         double rMaxPixel, rMaxDiff) {
+          double pixel = 0.0;
+          double [] pixelExtAngular
+             = new double[12] ;
+      } 
+
+} 
