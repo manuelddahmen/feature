90e6ea00fbd7ca33f433e775cff020781111091c
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-26 15:44:22 +0200

    Update IntuitiveRadialGradient.java

--- src/main/java/one/empty3/feature/IntuitiveRadialGradient.java
+++ src/main/java/one/empty3/feature/IntuitiveRadialGradient.java
@@ -8,9 +8,25 @@ public class IntuitiveRadialGradient extends PixM {
           for(int i=x-rMaxPixel; i<x+rMaxPixel; i++)
               for(int j=x-rMaxPixel; j<x+rMaxPixel; j++)
                   sum+= get(i, j) ;
-arc (int x, int y, double r1, double r2) ;
+          arc (int x, int y, double r1, double r2, 
+             double a1, double a2) ;
       } 
-
+    public Point2D pcircle(int x, int y, 
+              double angle, double r) {
+        return new Point2D(Math.cos(2*Math.PI*
+              angle),Math.sin(2*Math.PI*
+              angle)) 
+          .mult(r);
+    } 
+    public double arc (int x, int y, double r1, double r2, 
+              double a1, double a2) {
+         double eval = 0.0;
+        for(int i=x-r2; i<x+r2; i++)
+              for(int j=x-r2; j<x+r2; j++)
+                  if((eval=Math.sqrt((x-i)*(x-i)
+   +(y-j)* (y-j) ))<=r2 & & eval> r1) 
+                  sum+= get(i, j) ;
+     } 
       public double avgPixXY(int x, int y) {
           
       } 
