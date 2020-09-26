ed9a40ec887c469074d1004e6b9c31ef49b8b1f6
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-26 15:29:36 +0200

    Update IntuitiveRadialGradient.java

--- src/main/java/one/empty3/feature/IntuitiveRadialGradient.java
+++ src/main/java/one/empty3/feature/IntuitiveRadialGradient.java
@@ -1,11 +1,18 @@
 public class IntuitiveRadialGradient extends PixM {
+      private PixM pix;
       public double filter (double x, double y, 
          double rMaxPixel, rMaxDiff) {
           double pixel = 0.0;
           double [] pixelExtAngular
              = new double[12] ;
+          for(int i=x-rMaxPixel; i<x+rMaxPixel; i++)
+              for(int j=x-rMaxPixel; j<x+rMaxPixel; j++)
+                  sum+= get(i, j) ;
+arc (int x, int y, double r1, double r2) ;
       } 
 
-      
+      public double avgPixXY(int x, int y) {
+          
+      } 
 
 } 
