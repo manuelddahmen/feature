fcaa17cda9781336fc747f8871a38120ed40e730
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-27 20:11:47 +0200

    Update TestMatGrad.java

--- src/test/java/TestMatGrad.java
+++ src/test/java/TestMatGrad.java
@@ -3,12 +3,20 @@ import static org.junit.Assert.*;
 import javax.imageio.ImageIO;
 import java.io.File;
 import one.empty3.feature.*;
-
+import java.util.logging.*; 
 
 public class TestMatGrad {
+   static Logger logger;
+  static {
+    logger 
+
+            = Logger.getLogger(TestMatGrad.class.getName()); 
+
+  }
   @Test
   public void testMatGradAndDotProduct() {
    for(String fileStr :  new File("resources").list()) {
+     logger.info("start with " + fileStr);
     if(!fileStr.endsWith(".jpg"))
         continue;
      File file = new File("resources/"+fileStr);
@@ -21,12 +29,12 @@ public class TestMatGrad {
        // assertTrue(false);
       
      }
-        
+        logger.info("file loaded");
                 GradientFilter gradientMask = new GradientFilter(pixMOriginal.getColumns(), pixMOriginal.getLines());
                 M3 imgForGrad = new M3( pixMOriginal, 2, 2);
                 M3 filter = gradientMask.filter(imgForGrad);
                 PixM[][] imagesMatrix = filter.getImagesMatrix();//.normalize(0, 1);
-
+logger.info("gradient computed");
 
 //                    image1 = null;
 
@@ -35,12 +43,13 @@ public class TestMatGrad {
                         new PixM(pixMOriginal.getColumns(), pixMOriginal.getLines()));
                 linear.op2d2d(new char[]{'*'}, new int[][]{{1, 0}}, new int[]{2});
                 PixM smoothedGrad = linear.getImages()[2];
-
+logger.info("dot ootter product");
      LocalExtrema le =
     new  LocalExtrema( imagesMatrix[1][0].getColumns(), 
                       imagesMatrix[1][0].getLines(),
                       3, 2);
-     PixM pext = le.filter(new M3(imagesMatrix[0][0], 1, 1)).getImagesMatrix()[0][0];
+     le.filter(smoothedGrad);
+     logger.info("local maximum");
      
       AfterGradientBeforeExtremum a 
         = new AfterGradientBeforeExtremum(17);
@@ -51,12 +60,13 @@ public class TestMatGrad {
             pext, imagesMatrix[0][0], imagesMatrix[1][0]
          }}
       ));
-
+logger.info("angles tangentes");
      PixM pix = smoothedGrad;
         IntuitiveRadialGradient i 
          = new IntuitiveRadialGradient(pix);
      i.setMax(2., 5., 2, 12);
         PixM rad = pix.applyFilter(i);
+     logger.info("radial orientation");
         WriteFile.writeNext(file.getName()+"image reduite", pixMOriginal.normalize(0.,1.).getImage());
             WriteFile.writeNext(file.getName()+"image gradient gx", imagesMatrix[0][0].normalize(0.,1.).getImage());
       WriteFile.writeNext(file.getName()+"image gradient gy", imagesMatrix[1][0].normalize(0.,1.).getImage());
