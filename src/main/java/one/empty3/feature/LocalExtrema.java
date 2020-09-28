db8d877aa7749e206fce2a1d375727cdd187d013
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-09-28 15:39:09 +0200

    Update LocalExtrema.java

--- src/main/java/one/empty3/feature/LocalExtrema.java
+++ src/main/java/one/empty3/feature/LocalExtrema.java
@@ -53,39 +53,39 @@ public class LocalExtrema extends FilterMatPixM {
                 }
             }
         }*/
-        for(int c = 0; c< copy.getCompCount(); c++) {
+        //for(int c = 0; c< copy.getCompCount(); c++) {
             
-            original.setCompNo(c);
+            //original.setCompNo(c);
             for (int i = 0; i < original.columns; i++) {
                 for (int j = 0; j < original.lines; j++) {
-                    copy.setCompNo(c);
-                    boolean isMaximum = true;
+                    //copy.setCompNo(c);
+                    //boolean isMaximum = true;
                     double maxLocal = original.getIntensity(i, j, 0, 0);
                     int countOut = 0;
                     int countIn = 0;
                     if(maxLocal>=threshold) {
-                        //if (maxLocal > getThreshold()) {
+                        
                         for (int ii = -neighbourSize / 2; ii <= neighbourSize / 2; ii++) {
                             for (int ij = neighbourSize / 2; ij <= neighbourSize / 2; ij++) {
-                                double v = original.get(i + ii, j + ij, 0, 0);
+                                double v = original.getIntensity(i + ii, j + ij, 0, 0);
                                 if (v > maxLocal) {
                                     countIn++;
                                 }
                             }
                         }
-                        //}
-                        if (countIn <= pointsCount) {
+                    }
+                    if (countIn <= pointsCount) {
                             copy.setCompNo(0);
                             copy.set(i, j, 0, 0, 1);//1 au lieu value
                             copy.setCompNo(1);
                             copy.set(i, j, 0, 0, 1);//1 au lieu value
                             copy.setCompNo(2);
                             copy.set(i, j, 0, 0, 1);//1 au lieu value
-                        }
                     }
                 }
             }
         }
+        //}
         return copy;
 
     }
