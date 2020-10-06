4895044523c6a5f4944c9728762286882a76f983
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-06 08:56:56 +0200

    Create ProcessFile.java

---
+++ src/main/java/one/empty3/io/ProcessFile.java
@@ -0,0 +1,12 @@
+package one.empty3.io.ProcessFile;
+
+import java.io.File;
+
+public abstract class ProcessFile {
+     public ProcessFile(File in, File out) {
+     
+     
+     }
+     
+     public abstract boolean process();
+}
