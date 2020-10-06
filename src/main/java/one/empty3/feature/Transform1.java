d8cc3a5e3448f5fdf7258df3a93ecf0751fc354b
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-06 12:06:49 +0200

    Update Transform1.java

--- src/main/java/one/empty3/feature/Transform1.java
+++ src/main/java/one/empty3/feature/Transform1.java
@@ -4,5 +4,5 @@ import one.empty3.io.ProcessFile;
 import java.io.File;
 
 public class Transform1 extends ProcessFile {
-    public void process(File in, File out){}
+    public boolean process(File in, File out){}
 }
