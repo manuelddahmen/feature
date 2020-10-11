01c29556df453b569d17b0616819b8a5fa969ebb
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-11 14:52:39 +0200

    Update HarrisToPointInterest.java

--- src/main/java/one/empty3/feature/HarrisToPointInterest.java
+++ src/main/java/one/empty3/feature/HarrisToPointInterest.java
@@ -1,6 +1,7 @@
 package one.empty3.feature;
 
-public class HarrisToPointInterest extends FilterPixM {
+public class HarrisToPointInterest extends FilterMatPixM {
+    private M3 m;
     public HarrisToPointInterest(int c, int l) {
         super(c, l);
     }
@@ -17,10 +18,23 @@ public class HarrisToPointInterest extends FilterPixM {
      public void init(PixM i) {
          //GradientFilter gf  = new GradientFilter(1, 1.2);
          //M3 mat2x2n = i.applyFilter(gf);
+         m=i;
+       //  GaussFilter.getPixM(5, 5);
+         
+         
      }
      public double filter(double x, double y) {
-         //GradientFilter gf = new GradientFilter();
-         //PixM gx = 
-         return 0.0;
+         int i=(int)x, j=(int)y;
+         double gxgx, gxgy, gygx, gygy;
+         double gpgx = m.get(i+1, j,0,0)-m.get(i,j,0,0);
+         double gpgy = m.get(i, j+1,0,0)-m.get(i,j,0,0);
+         double gpgx1 = m.get(i, j,0,0)-m.get(i-1,j,0,0);
+         double gpgy1 = m.get(i, j,0,0)-m.get(i,j-1,0,0);
+         double gxgx = gpgx-gpgx1;
+         double gygx = gpgy1-gpgx;
+         double gxgy = gpgx1-gpgy;
+         double gygy = gpgy1-gpgy;
+         
+         return (gxgx+gygy)/(gxgx*gygy-gxgy*gygx);
      }
 }
