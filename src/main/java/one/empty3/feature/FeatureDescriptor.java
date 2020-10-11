b91816879d10778649040acc9a83460c85c47b24
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-11 15:57:27 +0200

    Update FeatureDescriptor.java

--- src/main/java/one/empty3/feature/FeatureDescriptor.java
+++ src/main/java/one/empty3/feature/FeatureDescriptor.java
@@ -1,5 +1,26 @@
 package one.empty3.feature;
 
-public class FeatureDescriptor {
-
+public class FeatureDescriptor extends FilterPixM {
+  private PixM m;
+  public FeatureDescriptor() {
+      super(5,5);
+  }
+  public boolean setDescription(double[][] values) {
+      return true;
+  }
+  public void setPixM(PixM m) {
+      this.m = m;
+  }
+  public double distance(FeatureDescriptor B,
+                         
+                         File [] set) {
+      return 1.0;
+  }
+  
+  public double distance(File set) {
+      return 1.0;
+  }
+  public double filter(double x, double y) {
+    
+  }
 }
