ee680515f3f3bb4877237b4271296eb484705c8d
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-11 16:13:21 +0200

    Update FeatureDescriptor.java

--- src/main/java/one/empty3/feature/FeatureDescriptor.java
+++ src/main/java/one/empty3/feature/FeatureDescriptor.java
@@ -1,7 +1,13 @@
 package one.empty3.feature;
 
-public class FeatureDescriptor extends FilterPixM {
+import one.empty3.library.Point3D;
+
+
+
+public class FeatureDescriptor extends PixM  {
   private PixM m;
+  private PixM pi;
+  private List<Point3D> poi;
   public FeatureDescriptor() {
       super(5,5);
   }
@@ -10,14 +16,13 @@ public class FeatureDescriptor extends FilterPixM {
   }
   public void setPixM(PixM m) {
       this.m = m;
-  }
-  public double distance(FeatureDescriptor B,
-                         
-                         File [] set) {
-      return 1.0;
+      HarrisToPointsIntetest h = new HarrisToPointsIntetest();
+      pi = m.applyFilter(h);;
+      poi = h.getPoi();
   }
   
-  public double distance(File set) {
+  
+  public double distance(FeatureDescriptor b, File [] set) {
       return 1.0;
   }
   public double filter(double x, double y) {
