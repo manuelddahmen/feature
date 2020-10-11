72508c29ffa4af9862754d4d983cbf54d2e124da
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-11 12:33:08 +0200

    Update FTPProcessFiles.java

--- src/main/java/one/empty3/FTPProcessFiles.java
+++ src/main/java/one/empty3/FTPProcessFiles.java
@@ -80,7 +80,7 @@ public class FTPProcessFiles {
         String [] classnamesArr = classnames.split(",");
       try {  
           int i=0;
-          currentDirin = "":
+          currentDirin = "";
           
         for (String classname2 : classnamesArr) {
             classname = classname2;
@@ -132,7 +132,7 @@ public class FTPProcessFiles {
             i++;
             
             
-            currentDirin = currentDirout:
+            currentDirin = currentDirout;
             
             }
         } catch (Exception ex) {
