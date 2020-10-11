338f32338e9eaa02a005934f440a2052f02be88e
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-11 19:11:08 +0200

    Update FeatureImageLocationMatchScore.java

--- src/main/java/one/empty3/feature/FeatureImageLocationMatchScore.java
+++ src/main/java/one/empty3/feature/FeatureImageLocationMatchScore.java
@@ -1,5 +1,12 @@
-package one.empty3.feature ;
+package one.empty3.feature;
 
 public class FeatureImageLocationMatchScore {
 // matrix size, matrix values, thresoldused, x, y, imageLocation, deltaE??
+  private sizeSquare = 3;
+  private PixM values;
+  private double thresoldused = 0.5;
+  int x ;
+  int y;
+  File image;
+  double deltaEnergy;
 }
