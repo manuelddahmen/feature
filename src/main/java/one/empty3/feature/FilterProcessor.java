363a1f77f5c195f28f2bde96a49b466605195df2
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-11 15:29:17 +0200

    Update FilterProcessor.java

--- src/main/java/one/empty3/feature/FilterProcessor.java
+++ src/main/java/one/empty3/feature/FilterProcessor.java
@@ -1,5 +1,7 @@
 package one.empty3.feature ;
 
+import one.empty3.io.ProcessFile;
+
 public abstract class FilterProcessor extends ProcessFile {
      public void initFilterProcessor (
          String filterPixMclassname, String filterPropertiesFilename
