f71a59d5a762e41fc7f0694bc7de65861651110e
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-11 15:10:30 +0200

    Create FilterProcessor.java

---
+++ src/main/java/one/empty3/feature/FilterProcessor.java
@@ -0,0 +1,10 @@
+package one.empty3.feature ;
+
+public abstract class FilterProcessor extends ProcessFile {
+     public void initFilterProcessor (
+         String filterPixMclassname, String filterPropertiesFilename
+     ) {
+     
+     } 
+
+} 
