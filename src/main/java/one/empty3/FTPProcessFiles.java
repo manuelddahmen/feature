37aad2dc851570313a5b1cee4c3bcbef61b74525
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-16 22:53:59 +0200

    Update FTPProcessFiles.java

--- src/main/java/one/empty3/FTPProcessFiles.java
+++ src/main/java/one/empty3/FTPProcessFiles.java
@@ -251,12 +251,12 @@ public class FTPProcessFiles {
                 
                 
                 process(file);
+            } else {
+                System.out.println("error file in not found");
+                System.exit(-1);
             }
         }
-        else {
-            System.out.println("error file in not found");
-            System.exit(-1);
-        }
+        
     }
     private static void printNames(String files[]) {
         if (files != null && files.length > 0) {
