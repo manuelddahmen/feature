68dcfaf97edcbc243a87d74afed80e634a3ff114
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-17 16:39:37 +0200

    Update GradProcess.java

--- src/main/java/one/empty3/feature/GradProcess.java
+++ src/main/java/one/empty3/feature/GradProcess.java
@@ -30,18 +30,17 @@ import java.util.logging.*;
      }
      M3 p = new M3(pixMOriginal.columns, pixMOriginal.lines,
                          2, 2);
-     PixM p = new PixM(3,3);
-     p.colorsRegion(0,0,3,3,
+     PixM p1 = new PixM(3,3);
+     p1.colorsRegion(0,0,3,3,
                    new double[]{
                       1.0,1.0,1.0,
                       1.0,1.0,1.0,
                       1.0,1.0,1.0
                                }
                    );
-     pattern.setMatrix(0,0,p);
+     p.setMatrix(0,0,p1);
     // pattern.setColorsRegion(0,1,0,3,3,0.0);
-     LocalPattern gf = new LocalPattern(pixMOriginal.columns, pixMOriginal.lines,
-                                         3, 2);
+     LocalPattern gf = new LocalPattern(p);
       
       PixM r = gf.filter(new M3(pixMOriginal, 1, 1)).getImagesMatrix()[0][0] ;
            try {
