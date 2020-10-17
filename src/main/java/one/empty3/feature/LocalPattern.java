69738b0e520d82bcddc9c75ae2b451af898311e0
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-17 10:07:36 +0200

    Update LocalPattern.java

--- src/main/java/one/empty3/feature/LocalPattern.java
+++ src/main/java/one/empty3/feature/LocalPattern.java
@@ -1,15 +1,26 @@
 package one.empty3.feature ;
 
-public class LocalPattern extends FilterPixM {
-    public LocalPattern (int n) {
+public class LocalPattern extends FilterMatPixM {
+    /***
+        Ligne 1 searchPattern value = x [0,1] 
+           - 1 : ignore 
+        Ligne 2 replacePattern value x' [0,1] 
+        comp4 => rgb' = rgb *(1-opacity) si x ! =-1
+       
+    */
+    public LocalPattern (M3 searchReplace) {
         super(n, n) ;
     } 
     /***
     */
-    public PixM searchSet(
-           PixM mat, 
-           M3 searchReplace, 
+    public PixM filter(
+           
+           M3 m
            ) {
-         return mat.copy();
+         M3 mat = mat.copy();
+        
+        
+        
+        return mat;
     } 
 } 
