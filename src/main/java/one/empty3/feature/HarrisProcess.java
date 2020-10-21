3f785ffd2f02efc07c3b7b9268c4714da22c1999
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-21 14:04:17 +0200

    Update HarrisProcess.java

--- src/main/java/one/empty3/feature/HarrisProcess.java
+++ src/main/java/one/empty3/feature/HarrisProcess.java
@@ -13,5 +13,30 @@ import java.util.logging.Logger;
 import one.empty3.library.core.lighting.Colors;
 public class HarrisProcess extends ProcessFile {
      public HarrisProcess (){}
+     
+     public boolean process(File in, File out) {
+      try {
+        
+        
+           PixM imageCoutours = PixM.getPixM(ImageIO.read(in), 500.0);
+        
+           this.m = imageCoutours;
+        
+           BufferedImage img = m.getImage();
+        
+           PixM  m2 = new PixM(m.columns, m.lines);
+           
+           HarrisToPointsOfInterest h = new HarrisToPointsOfInterest();
+           
+           m2 = m.applyFilter(h);
+           
+        
+           ImageIO.write(m2.getImage(), "JPEG", out);
+           
+           
+      } catch() {
+           ex.printStackTrace();
+      }
+          
 
 } 
