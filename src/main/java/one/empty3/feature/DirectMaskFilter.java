672249f7a81baedcc376f874026a7ad567cc104b
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-24 13:16:01 +0200

    Update DirectMaskFilter.java

--- src/main/java/one/empty3/feature/DirectMaskFilter.java
+++ src/main/java/one/empty3/feature/DirectMaskFilter.java
@@ -1,8 +1,17 @@
 Package one.empty3.feature ;
 public class DirectMaskFilter {
-    public DirectMaskFilter(PixM[] ms)
+    PixM m1; PixM m2;
+    public DirectMaskFilter(PixM m1, PixM m2)
     {
          
+        this.m1 = m1;
+        this.m2 = m2;
     }
+    /* (M3.p =) = p1x, p1y, 
+    
+, p2x, p2y, p1, p2, c0,c1,c2,alpha, */
+    public void applyOperator(String formula) {
+        
+    } 
 
 }
