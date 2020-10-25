20bf9a5cbd1ffef32825bec51f4ae87b5eec58c7
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-25 17:51:27 +0100

    Update ProxyValue.java

--- src/main/java/one/empty3/feature/ProxyValue.java
+++ src/main/java/one/empty3/feature/ProxyValue.java
@@ -3,6 +3,20 @@ import java.io.File ;
 public class Proxy Value extends ProcessFile {
 
     public boolean process (File in, File out) {
+        
+        
+         for (int i = 0; i < original.columns; i++)
+
+                for (int j = 0; j < original.lines; j++)
+                    if(original.getIntensity(i,j)){
+                        
+                    }
+                    for (int c = 0; c < 4; c++) {
+
+                    }
+
+                }
+          }
         return false;
     } 
 
