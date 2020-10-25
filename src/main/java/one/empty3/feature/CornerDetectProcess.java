994779117793e3bf05cfeeed32ace9e1b3f7476a
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-25 13:28:34 +0100

    Create CornerDetectProcess.java

---
+++ src/main/java/one/empty3/feature/CornerDetectProcess.java
@@ -0,0 +1,8 @@
+package one.empty3.feature ;
+import java.io.File;
+public class CornerDetectProcess extends ProcessFile {
+    public boolean process(File in, File out) {
+    return true;
+    
+    } 
+} 
