60163ee4cf692747d210efe9a15c09f0d9b9a7d0
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-25 17:40:54 +0100

    Create ProxyValue.java

---
+++ src/main/java/one/empty3/feature/ProxyValue.java
@@ -0,0 +1,9 @@
+package one.empty3.feature ;
+import java.io.File ;
+public class Proxy Value extends ProcessFile {
+
+    public boolean process (File in, File out) {
+        return false;
+    } 
+
+} 
