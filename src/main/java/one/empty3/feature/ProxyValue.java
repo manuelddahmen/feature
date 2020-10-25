23f203782d52c3730668442b8dbce6e1d6b8d95d
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-25 19:00:55 +0100

    Update ProxyValue.java

--- src/main/java/one/empty3/feature/ProxyValue.java
+++ src/main/java/one/empty3/feature/ProxyValue.java
@@ -9,22 +9,45 @@ public class Proxy Value extends ProcessFile {
          for (int i = 0; i < original.columns; i++)
 
                 for (int j = 0; j < original.lines; j++)
-                  if(original.getIntensity(i,j)>0.1){
+                  if(original.getIntensity(i,j)>0.5){
                         
                     
                     for (int c = 0; c < 4; c++) {
-                        searchFromTo(original, i, j, 0.1, 1.0).forEach(
-                           p -> {
-                           }
-                        );
+                        original.setCompNo(c);
+                        Point3D p = searchFromTo(original, i, j, 0.5, 1.0);
+                        
+                           
+                       
                     }
                   }
-                }
+                
           }
         return false;
     } 
-    public Point2D searchFromTo(
+    public Point3D searchFromTo(
            PixM original, int i, int j, double min, value) {
+        Point3D p = null;
+        for(int i2=0; j2<original.columns/2; i2++)
+            for(int j2=0; j2<original.columns/2; j2++)
+                { 
+                    if(original.get(i+i2, j+j2)>= min) {
+                        p = new Point3D(i-i2, j-j2, original.get(i-i2,j-j2));
+                    }
+                if(original.get(i+i2, j-j2)>= min) {
+                        p = new Point3D(i+i2, j-j2, original.get(i+i2,j-j2));
+                    }
+                if(original.get(i-i2, j+j2)>= min) {
+                        p = new Point3D(i-i2, j+j2, original.get(i-i2,j+j2));
+                    }
+                if(original.get(i-i2, j-j2)>= min) {
+                        p = new Point3D(i+i2, j-j2, original.get(i-i2,j-j2));
+                    }
+                
+                ifp!=null)
+                    return p;
+             }
+          }
+        
         return null;
     }
 } 
