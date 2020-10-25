249dc438c6c4a0418f724537087d9de531ca126a
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-25 19:05:53 +0100

    Update ProxyValue.java

--- src/main/java/one/empty3/feature/ProxyValue.java
+++ src/main/java/one/empty3/feature/ProxyValue.java
@@ -1,11 +1,27 @@
 package one.empty3.feature ;
+
+
 import java.io.File ;
-import one.empty3.library.Point2D;
+import one.empty3.library.Point3D;
+
+
 public class Proxy Value extends ProcessFile {
 
     public boolean process (File in, File out) {
         
-        
+    if(!in.getName().endsWith(".jpg"))
+        return false;
+    File file = in;
+    PixM original = null;
+    try {
+        original = PixM.getPixM(ImageIO.read(file), 500.0);
+    } catch(Exception ex) {
+        ex.printStackTrace();
+      return false;
+       // assertTrue(false);
+      
+     }
+        PixM copy = original.copy();
          for (int i = 0; i < original.columns; i++)
 
                 for (int j = 0; j < original.lines; j++)
@@ -15,14 +31,26 @@ public class Proxy Value extends ProcessFile {
                     for (int c = 0; c < 4; c++) {
                         original.setCompNo(c);
                         Point3D p = searchFromTo(original, i, j, 0.5, 1.0);
-                        
+                        if(p!=null) {
+                             copy.set(i, j, p.getZ());
+                        }
                            
                        
                     }
                   }
                 
           }
-        return false;
+    
+    
+    try {
+       ImageIO.write( copy.getImage(), "jpg", out);
+     } catch (Exception ex){
+         return false;
+     }
+       
+       System.gc();
+     
+        return true;
     } 
     public Point3D searchFromTo(
            PixM original, int i, int j, double min, value) {
