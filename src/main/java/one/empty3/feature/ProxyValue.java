74e67fa88e969271d6ed4cb57364848768e8002d
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-25 18:42:32 +0100

    Update ProxyValue.java

--- src/main/java/one/empty3/feature/ProxyValue.java
+++ src/main/java/one/empty3/feature/ProxyValue.java
@@ -1,5 +1,6 @@
 package one.empty3.feature ;
 import java.io.File ;
+import one.empty3.library.Point2D;
 public class Proxy Value extends ProcessFile {
 
     public boolean process (File in, File out) {
@@ -8,16 +9,22 @@ public class Proxy Value extends ProcessFile {
          for (int i = 0; i < original.columns; i++)
 
                 for (int j = 0; j < original.lines; j++)
-                    if(original.getIntensity(i,j)){
+                  if(original.getIntensity(i,j)>0.1){
                         
-                    }
+                    
                     for (int c = 0; c < 4; c++) {
-
+                        searchFromTo(original, i, j, 0.1, 1.0).forEach(
+                           p -> {
+                           }
+                        );
                     }
-
+                  }
                 }
           }
         return false;
     } 
-
+    public Point2D searchFromTo(
+           PixM original, int i, int j, double min, value) {
+        return null;
+    }
 } 
