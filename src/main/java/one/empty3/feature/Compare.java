5f772033ae549ac14a8c557ea984330ab4d554d0
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-02 19:55:07 +0100

    Update Compare.java

--- src/main/java/one/empty3/feature/Compare.java
+++ src/main/java/one/empty3/feature/Compare.java
@@ -3,5 +3,9 @@ package one.empty3.feature ;
 import one.empty3.io.ProcessFile ;
 import java.io.File ;
 public class Compare extends ProcessFile {
-     public boolean process (File in, File out ) 
+     public boolean process (File in, File out ){
+        PixM p = PixM.getPixM() ;
+PixM p2 = PixM.getPixM() ;
+new DiffE().dist(p, p2)
+} 
 } 
