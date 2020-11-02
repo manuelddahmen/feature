6290b55b04b625cff43968c7acd74678650b28fe
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-02 09:44:17 +0100

    Create DiffE.java

---
+++ src/main/java/one/empty3/feature/DiffE.java
@@ -0,0 +1,12 @@
+public class DiffE {
+    class ColorTranform {}
+    private PixM p1, PixM p2;
+    public DiffE(PixM p1, PixM p2){
+        this.p1=p1;
+        
+        this.p2=p2;
+    }
+    
+    public double dist(Rectangle, Rectangle, ColorTransform
+    ) {}
+}
