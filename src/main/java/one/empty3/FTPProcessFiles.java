800df306e0e4248e9dd883c89a57cc8a42cf9a33
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-02 02:29:24 +0100

    Update FTPProcessFiles.java

--- src/main/java/one/empty3/FTPProcessFiles.java
+++ src/main/java/one/empty3/FTPProcessFiles.java
@@ -119,12 +119,12 @@ public class FTPProcessFiles {
           if(class0==null || class0.equals("")){
               sep=""; } else sep=",";
           
-           {
+     catch(Exception ex)  {
                 classnames =(classnames !=null?
                              classnames+sep:"")
                            
                              +(class0==null?"":""+class0);
-            }
+      }
           if(i==0&&(class0==null || class0.equals(""))
            String [] classnamesArr = classnames.split(",");
            for (String classname2 : classnamesArr) {
