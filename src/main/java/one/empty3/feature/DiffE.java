0848df25e9ad00f93d6208a7bce5897d256257d1
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-02 09:53:36 +0100

    Update DiffE.java

--- src/main/java/one/empty3/feature/DiffE.java
+++ src/main/java/one/empty3/feature/DiffE.java
@@ -1,5 +1,13 @@
 public class DiffE {
-    class ColorTranform {}
+    class ColorTranform {
+        Circle ref;
+        public void rotate(){}
+        public double compare(ColorTransform cf){}
+    }
+    class Circle {
+         PixM p;
+        double x, y, r
+    }
     private PixM p1, PixM p2;
     public DiffE(PixM p1, PixM p2){
         this.p1=p1;
@@ -7,6 +15,6 @@ public class DiffE {
         this.p2=p2;
     }
     
-    public double dist(Rectangle, Rectangle, ColorTransform
+    public double dist(Circle c1, Circle c2, ColorTransform cf
     ) {}
 }
