104cab16e1c2672556cf46a6fdc239722e97f5ef
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-04 10:03:13 +0100

    Update DiffE.java

--- src/main/java/one/empty3/feature/DiffE.java
+++ src/main/java/one/empty3/feature/DiffE.java
@@ -4,6 +4,7 @@ import one.empty3.library.Point2D;
 import java.awt.Color;
 
 public class DiffE {
+private int sizeElement = 20;
     class ColorTranform {
         Circle ref;
         public void rotate(){}
@@ -93,12 +94,12 @@ public class DiffE {
         
     int tri = 100;
     public double[][] dist(PixM p1, PixM p2) {
-       double dist [] = new double[20*20];
+       
        for(double g=0; g<p1.columns;  g++) {
            for(int h=0; h<p1.lines;  h++) {
-       Circle cij = new Circle(p1, g*p1,columns/20., 1.*h*p1.lines/20., 20.);
-       Circle ci1 = new Circle(p2, g*p2,columnns/20., 1.*h*p2.lines/20., 20.);  
-       Circle cir = new Circle(p2, g*r*p2.columns/20., 1.*h*r*p2.lines/20., 20.);
+       Circle cij = new Circle(p1, g*p1.columns/elementSize., 1.*h*p1.lines/elementSize,, elementSize);
+       Circle ci1 = new Circle(p2, g*p2.columns/elementSize, 1.*h*p2.lines/elementSize, elementSize);  
+       Circle cir = new Circle(p2, g*r*p2.columns/elementSize, 1.*h*r*p2.lines, elementSize, elementSize);
                                
        double[] [] di1, di2;
                // try min diff
