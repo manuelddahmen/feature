48c7374eccdb1928406e6d17467aecd6d81d1d41
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-04 22:42:40 +0100

    Update JsonProps.java

--- src/main/java/one/empty3/feature/JsonProps.java
+++ src/main/java/one/empty3/feature/JsonProps.java
@@ -1,7 +1,7 @@
 public class JsonProps {
-    public void defProcess (String file) {
+    //*public void defProcess (String file) {
     
-
+/*
 String jsonString = file.getContents(); //assign your JSON String here
 JSONObject obj = new JSONObject(jsonString);
 if(String in = obj.getJSONObject("in")) {
@@ -16,7 +16,119 @@ for(int j = 0; i<filter.get(i).names().length(); j++){
 }
          
        }
-      
+      *[[public Circle getLevel(Histogram2.Circle c) {
+        // I mean. Parcourir le cercle
+        // mesurer I / numPoints
+        // for(int i=Math.sqrt()
+        //  return c;
+        int count = 0;
+        double intensity = 0.0;
+        for (double i = c.x-c.r; i <= c.x+c.r; i++) {
+            for (double j = c.y-c.r; j <= c.y+c.r; j++) {
+                if (Math.sqrt((i-c.x) * (i-c.x) + (j-c.y) * ( j-c.y)) <= c.r*c.r
+                        && c.x-c.r>=0 && c.y-c.r>=0 && c.x+c.r<m.columns && c.y+c.r<m.lines) {
+                    intensity += m.getIntensity((int) i, (int) j);
+                    count++;
+                }
+            }
+        }
 
+        if(count>0) {
+            c.i = intensity / count;
+        }
+        else {
+            c.i = 0.0;
+            c.r = 0} 
+public Circle getLevel(Histogram2.Circle c) {
+        // I mean. Parcourir le cercle
+        // mesurer I / numPoints
+        // for(int i=Math.sqrt()
+        //  return c;
+        int count = 0;
+        double intensity = 0.0;
+        for (double i = c.x-c.r; i <= c.x+c.r; i++) {
+            for (double j = c.y-c.r; j <= c.y+c.r; j++) {
+                if (Math.sqrt((i-c.x) * (i-c.x) + (j-c.y) * ( j-c.y)) <= c.r*c.r
+                        && c.x-c.r>=0 && c.y-c.r>=0 && c.x+c.r<m.columns && c.y+c.r<m.lines) {
+                    intensity += m.getIntensity((int) i, (int) j);
+                    count++;
+                }
+            }
+        }
 
-}
+        if(count>0) {
+            c.i = intensity / count;
+        }
+        else {
+            c.i = 0.0;
+            c.r = 0;public Circle getLevel(Histogram2.Circle c) {
+        // I mean. Parcourir le cercle
+        // mesurer I / numPoints
+        // for(int i=Math.sqrt()
+        //  return c;
+        int count = 0;
+        double intensity = 0.0;
+        for (double i = c.x-c.r; i <= c.x+c.r; i++) {
+            for (double j = c.y-c.r; j <= c.y+c.r; j++) {
+                if (Math.sqrt((i-c.x) * (i-c.x) + (j-c.y) * ( j-c.y)) <= c.r*c.r
+                        && c.x-c.r>=0 && c.y-c.r>=0 && c.x+c.r<m.columns && c.y+c.r<m.lines) {
+                    intensity += m.getIntensity((int) i, (int) j);
+                    count++;
+                }
+            }
+        }
+
+        if(count>0) {
+            c.i = intensity / count;
+        }
+        else {
+            c.i = 0.0;
+            c.r = 0;public Circle getLevel(Histogram2.Circle c) {
+        // I mean. Parcourir le cercle
+        // mesurer I / numPoints
+        // for(int i=Math.sqrt()
+        //  return c;
+        int count = 0;
+        double intensity = 0.0;
+        for (double i = c.x-c.r; i <= c.x+c.r; i++) {
+            for (double j = c.y-c.r; j <= c.y+c.r; j++) {
+                if (Math.sqrt((i-c.x) * (i-c.x) + (j-c.y) * ( j-c.y)) <= c.r*c.r
+                        && c.x-c.r>=0 && c.y-c.r>=0 && c.x+c.r<m.columns && c.y+c.r<m.lines) {
+                    intensity += m.getIntensity((int) i, (int) j);
+                    count++;
+                }
+            }
+        }
+
+        if(count>0) {
+            c.i = intensity / count;
+        }
+        else {
+            c.i = 0.0;
+            c.r = 0;
+}}public Circle getLevel(Histogram2.Circle c) {
+        // I mean. Parcourir le cercle
+        // mesurer I / numPoints
+        // for(int i=Math.sqrt()
+        //  return c;
+        int count = 0;
+        double intensity = 0.0;
+        for (double i = c.x-c.r; i <= c.x+c.r; i++) {
+            for (double j = c.y-c.r; j <= c.y+c.r; j++) {
+                if (Math.sqrt((i-c.x) * (i-c.x) + (j-c.y) * ( j-c.y)) <= c.r*c.r
+                        && c.x-c.r>=0 && c.y-c.r>=0 && c.x+c.r<m.columns && c.y+c.r<m.lines) {
+                    intensity += m.getIntensity((int) i, (int) j);
+                    count++;
+                }
+            }
+        }
+
+        if(count>0) {
+            c.i = intensity / count;
+        }
+        else {
+            c.i = 0.0;
+            c.r = 0;
+}}😎*/
+*[
+    M
