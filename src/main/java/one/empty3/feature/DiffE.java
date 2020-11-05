13a1213c57635a79553dd37b410874cbf22aa3b1
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-05 13:12:13 +0100

    Update DiffE.java

--- src/main/java/one/empty3/feature/DiffE.java
+++ src/main/java/one/empty3/feature/DiffE.java
@@ -66,9 +66,9 @@ private int sizeElement = 20;
         }
         public double get(double r) {
             double pi = 0.0;
-            for (double a = 0; a<r; a+= 1/r/2/Math.PI)
+            for (double a = 0; a<r; a+= 1/r/2/Math.PI) {
             
-            pi+= m.getIntensity((int)(r*Math.cos(a)) ,(int)(r*Math.sin(a)));
+            pi+= m.getIntensity((int)(r*Math.cos(a)) ,(int)(r*Math.sin(a)));}
             return pi;
         }
         
@@ -91,7 +91,7 @@ private int sizeElement = 20;
                        return diff; // attention red
                 
         } 
-} 
+
 public Circle getLevel(Circle c) {
         // I mean. Parcourir le cercle
         // mesurer I / numPoints
