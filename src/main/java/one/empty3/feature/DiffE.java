edba094b11f6a780b6411868c4927ace2f4ae232
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-07 16:48:14 +0100

    Update DiffE.java

--- src/main/java/one/empty3/feature/DiffE.java
+++ src/main/java/one/empty3/feature/DiffE.java
@@ -89,8 +89,8 @@ private int sizeElement = 20;
             Point3D diff = Point3D.n(0,0,0);
               
             for(int i = -b.x+ b.r; i<b.y+b.r;i++)
-                double ls =new Point3D(p2.getRgb((int)i. (int)j),0.).norme());
-                double ml moins =new Point3D(p1.getRgb((int)i (int)j)).norme();
+                double ls =new Point3D(p2.getRgb((int)i. (int)j),0.).norme();
+                double ml moins =new Point3D(p1.getRgb((int)i (int)j),0.0).norme();
                    diff =cols.moins(moins);
                 for(int j= -b.x+ b.r; j<b.y+b.r; j++) {
                     
