d9a385f9f1aa4a2ec4b2c93897619dc15064af34
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-07 20:58:21 +0100

    Update DiffE.java

--- src/main/java/one/empty3/feature/DiffE.java
+++ src/main/java/one/empty3/feature/DiffE.java
@@ -30,33 +30,7 @@ private int sizeElement = 20;
         public double get(double r, double g, double b){
            return Math.sqrt(r*r+g*g+b*b);
             }
-         public double getLevel(Circle c,Histogram2.Circle c) {
-        // I mean. Parcourir le cercle
-        // mesurer I / numPoints
-        // for(int i=Math.sqrt()
-        //  return c;
-        int count = 0;
-        double intensity = 0.0;
-        for (double i = c.x-c.r; i <= c.x+c.r; i++) {
-            for (double j = c.y-c.r; j <= c.y+c.r; j++) {
-                if (Math.sqrt((i-c.x) * (i-c.x) + (j-c.y) * ( j-c.y)) <= c.r*c.r
-                        && c.x-c.r>=0 && c.y-c.r>=0 && c.x+c.r<m.columns && c.y+c.r<m.lines) {
-                    intensity += p.getIntensity((int) i, (int) j);
-                    count++;
-                }
-            }
-        }
-
-        if(count>0) {
-            c.i = intensity / count;
-            
-        }
-        else {
-            c.i = 0.0;
-            c.r = 0.0;
-        } 
-        return c.r/c.i;
-     }
+         
         
          -
          
