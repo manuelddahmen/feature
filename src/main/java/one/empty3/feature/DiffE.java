1a243e6ed58ee973ee58e5b0fb32cdc8fcdbe8a2
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-09 13:44:49 +0100

    Update DiffE.java

--- src/main/java/one/empty3/feature/DiffE.java
+++ src/main/java/one/empty3/feature/DiffE.java
@@ -179,13 +179,14 @@ List<Point> candidate = new ArrayList() ;
        for(int  g=0; g<p1.columns;  g++) {
            for(int h=0; h<p1.lines;  h++) {
        
-       Circle cir[g][h] [0] =new Circle(g*p2.columns/elementSize, 1.*h*p2.lines/elementSize, elementSize);  
+       Circle cir[g][h] [0] =new Circle(g*p1.columns/elementSize, 1.*h*p1.lines/elementSize, elementSize);  
+       Circle cil[g][h] [1] =new Circle(g*p2.columns/elementSize, 1.*h*p2.lines/elementSize, elementSize);  
 
                                
     
            
 
-       
+    
 
         
         
