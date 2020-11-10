b664ae4b03a76365fd3e6be3ffae1399fb13f1d8
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-10 16:46:58 +0100

    Update PixM.java

--- src/main/java/one/empty3/feature/PixM.java
+++ src/main/java/one/empty3/feature/PixM.java
@@ -22,7 +22,7 @@ public class PixM extends M {
         for (int i = 0; i < image.getWidth(); i++) {
             for (int j = 0; j < image.getHeight(); j++) {
                 int rgb = image.getRGB(i, j);
-                float[] colorComponents = new float[getCompCount();
+                float[] colorComponents = new float[getCompCount()];
                 colorComponents = new Color(rgb).getColorComponents(colorComponents[com]);
                 for (int com = 0; com < getCompCount(); com++) {
                     setCompNo(com);
