9f97a0844592b620a320af04d925889a8db0b8fd
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-20 22:03:22 +0100

    Update DiffE3.java

--- src/main/java/one/empty3/feature/pio/DiffE3.java
+++ src/main/java/one/empty3/feature/pio/DiffE3.java
@@ -197,9 +197,9 @@ private int sizeElement = 20, elementSize=20;
                         for(int i1=0; i1<pi.getColumns(); i1++)
 
                 for(int j1=0; j1<pi.getLines(); j1++) {
-                       moutA.set(i,j,moutA.get(i,j)+ match(new Circle(pi,i,j,0,r), new Circle(pi, i1, j1,0,r)));
+                       moutA.set(i,j,moutA.get(i,j)+ new Circle(pi,i,j,0,r).match(new Circle(pi, i1, j1,0,r)));
                                              
-                moutB.set(i1,j1,moutB.get(i1,j1)+match(new Circle(pi,i1,j1,0,r),new Circle(pi,i,j,0,r)));
+                moutB.set(i1,j1,moutB.get(i1,j1)+new Circle(pi,i1,j1,0,r).match(new Circle(pi,i,j,0,r)));
 
                 
                         }
