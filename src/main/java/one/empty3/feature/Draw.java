fb743cce1b93f4e9ecb4bc8e2ad135de8b41597d
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-21 16:28:25 +0100

    Update Draw.java

--- src/main/java/one/empty3/feature/Draw.java
+++ src/main/java/one/empty3/feature/Draw.java
@@ -1,7 +1,18 @@
 package one.empty3.feature;
-
-public class Draw extends FilterPixM {
+import one.empty3.io.*;
+import java.io.File;
+import javax.imageio.ImageIO;
+public class Draw extends ProcessFile {
     public Draw(){}
-    public double filter(int x, int y) {
+    
+    }
+
+    public boolean process(File in, File out) {
+        try {
+         ImageIO.write(ImageIO.read(in), "jpg", out);
+          
+              
+              
+       
     }
 }
