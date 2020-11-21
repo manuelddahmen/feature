e0751997052c247c5878eeaebeda6903357bb1d1
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-11-21 21:04:55 +0100

    Update ProcessFile.java

--- src/main/java/one/empty3/io/ProcessFile.java
+++ src/main/java/one/empty3/io/ProcessFile.java
@@ -2,7 +2,7 @@ package one.empty3.io;
 
 import java.io.File;
 import java.util.*;
-
+import one.empty3.feature.*;
 public abstract class ProcessFile {
      List<PixM> listImage = new ArrayList<>(); 
      public ProcessFile() {
