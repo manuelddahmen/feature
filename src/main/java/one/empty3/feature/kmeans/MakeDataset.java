dd05972f1365890135c789893dbf2842f26b80db
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-12-06 08:51:53 +0100

    Create MakeDataset.java

---
+++ src/main/java/one/empty3/feature/kmeans/MakeDataset.java
@@ -0,0 +1,13 @@
+package one.empty3.feature.kmeans ;
+
+import java.io.*;
+
+/***
+ line : x, y, r, g, b
+*/
+public class MakeDataset {
+    public MakeDataset (File image, File outputCsv) {
+        
+    } 
+
+} 
