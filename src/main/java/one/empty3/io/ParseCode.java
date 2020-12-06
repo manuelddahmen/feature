ce69211730b609e314d217f35f6ed9e7015dfbef
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-12-06 11:29:22 +0100

    Update ParseCode.java

--- src/main/java/one/empty3/io/ParseCode.java
+++ src/main/java/one/empty3/io/ParseCode.java
@@ -2,6 +2,7 @@ package one.empty3.io;
 
 public class ParseCode {
     public void parse(String str) {
+       // comment 
        // keyword* name* defChar, exprChar
        // literal( char, String, double, integer, 
        // boolean ) 
