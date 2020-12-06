274a725ba94331f6497e8e96cefd25cd40641e67
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-12-06 10:42:44 +0100

    Create ParseCode.java

---
+++ src/main/java/one/empty3/io/ParseCode.java
@@ -0,0 +1,9 @@
+package one.empty3.io;
+
+public class ParseCode {
+    public void parse(String str) {
+       // keyword* name* defChar, exprChar
+       // literal( char, String, double, integer, 
+       // boolean ) 
+    } 
+}
