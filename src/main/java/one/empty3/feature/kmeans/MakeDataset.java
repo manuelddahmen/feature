4d7124bb0f358be96854b2c2bb7eaeda6445d3e0
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-12-06 09:20:53 +0100

    Update MakeDataset.java

--- src/main/java/one/empty3/feature/kmeans/MakeDataset.java
+++ src/main/java/one/empty3/feature/kmeans/MakeDataset.java
@@ -1,9 +1,10 @@
 package one.empty3.feature.kmeans ;
 
 import java.io.*;
-
+import one.empty3.io.ProcessFile ;
+import one.empty3.feature.*;
 /***
- line : x, y, r, g, b
+ line : l, c, r, g, b
 */
 public class MakeDataset {
     public MakeDataset (File image, File outputCsv) {
@@ -11,6 +12,22 @@ public class MakeDataset {
           (image) ;
         PixM pix = new PixM(img) ;
 
+
+        PrintWriter pw = new PrintWriter(output) ;
+        for(int l=0; l<pix.getLines(); l++) 
+         for(int c=0; c<pix.getColumns(); c++) {
+          pix. setCompNo(0) ;
+          double r = pix.get(l, c) ;
+
+          pix. setCompNo(1) ;
+          double g = pix.get(l, c) ;
+
+          pix. setCompNo(2) ;
+          double b = pix.get(l, c) ;
+
+          pw.println("" +l+" " +c+" " +
+            +" "+ r+"" + g + " " + b) ;
+         } 
     } 
 
 } 
