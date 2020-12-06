5636c5e0aab7b876cbedf68bd5988104c6bca939
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-12-06 19:30:28 +0100

    Update KMeans.java

--- src/main/java/one/empty3/feature/KMeans.java
+++ src/main/java/one/empty3/feature/KMeans.java
@@ -4,7 +4,7 @@ import one.empty3.io.ProcessFile;
 import java.io.File ;
 
 public class KMeans extends ProcessFile {
-    public bolean process(File in, File out) {
+    public boolean process(File in, File out) {
         return false;
     }
 }
