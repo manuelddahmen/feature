28bc038774c716afc6b389de2fbfcddaf53e6069
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-12-11 13:32:00 +0100

    Update Threshold.java

--- src/main/java/one/empty3/feature/Threshold.java
+++ src/main/java/one/empty3/feature/Threshold.java
@@ -1,8 +1,11 @@
 package one.empty3.feature.kmeans.*;
-
-public class FilterPixM {
+import one.empty3.feature.*;
+public class Threshold {
     public double filter(double x, double y) {
         double d = 0.0;
+        int size = 0;
+        double sigma = 1.2;
+        
         if(getIntensity(x,y)>0.8)
             return 1.0;
         return 0.0;
