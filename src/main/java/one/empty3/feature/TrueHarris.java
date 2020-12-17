9b7fb1493504c58116636c9408dae775c7e7cf0e
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-12-17 14:49:17 +0100

    Update TrueHarris.java

--- src/main/java/one/empty3/feature/TrueHarris.java
+++ src/main/java/one/empty3/feature/TrueHarris.java
@@ -11,13 +11,13 @@ double Sx2 = (  (get(i+1, j)-get(i,j)) -(get(i,j)-get(i-1,j)) )  *get(i,j) ;
 double Sy2 = (  (get(i, j+1)-get(i,j)) -(get(i,j)-get(i,j-1)) )  *get(i,j) ; 
 double Ix = gx*get(i, j);
 double Iy = gy*get(i, j);
-Ix2 = Ix*Ix;
-Ixy = Ix*Iy;
-double Sx2 = gx*Ix;
-double Sy2 = gy*Iy;
+double Ix2 = Ix*Ix;
+double Ixy = Ix*Iy;
+double Ix2 = gx*Ix;
+double Iy2 = gy*Iy;
 double Sxy = Math.sqrt((gx+gy)/2*get(i, j)) ;// Robert Collins 
 
-double r = (Sx2*Sy2-Sxy*Sxy) /(Sx2+Sy2);
+double r = (Ix2*Iy2-Ixy*Ixy) /(Ix2+Iy2);
              
    return r;
   } 
