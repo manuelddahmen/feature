ff14f0f698b808c956498dc7c6d6c2ff8986ac10
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-12-17 14:05:50 +0100

    Update TrueHarris.java

--- src/main/java/one/empty3/feature/TrueHarris.java
+++ src/main/java/one/empty3/feature/TrueHarris.java
@@ -4,8 +4,8 @@ public class TrueHarris extends FilterPixM {
 
 public double filter(int i, int j) {
 double gx=get(i+1)-get(i,j), gy=get(i,j+1)+gey(i,j);
-double Sx2 = (  ( (gx=(get(i+1, j)-get(i,j)) )-(get(i,j)-get(i-1,j)) )  *get(i"j) ;
-double Sy2 = (  ( (gy=(get(i, j+1)-get(i,j)) )-(get(i,j)-get(i,j-1)) )  *get(i, j) ; 
+double Sx2 = (  ( (gx=(get(i+1, j)-get(i,j)) )-(get(i,j)-get(i-1,j)) )  *get(i,j) ;
+double Sy2 = (  ( (gy=(get(i, j+1)-get(i,j)) )-(get(i,j)-get(i,j-1)) )  *get(i,j) ; 
 double Ix = gx*get(i, j);
 double Iy = gy*get(i, j);
 Ix2 = Ix*Ix;
