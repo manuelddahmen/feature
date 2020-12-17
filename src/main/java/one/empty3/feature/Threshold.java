55235e95e9f5e1e73f3f87e21a97a5a26cdfdfb5
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-12-17 10:53:17 +0100

    Update Threshold.java

--- src/main/java/one/empty3/feature/Threshold.java
+++ src/main/java/one/empty3/feature/Threshold.java
@@ -16,7 +16,7 @@ public class Threshold {
 GaussFilterPixM gauss = new GaussFilterPixM(size, sigma);
 
        
-        if(getIntensity(x,y)>0.8)
+        if(gauss.getIntensity(x,y)>0.8)
             return 1.0;
         return 0.0;
     }
