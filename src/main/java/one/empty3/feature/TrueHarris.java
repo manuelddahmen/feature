6fdc0335df04dbd022f6c69d45da811075396377
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-12-17 14:16:24 +0100

    Update TrueHarris.java

--- src/main/java/one/empty3/feature/TrueHarris.java
+++ src/main/java/one/empty3/feature/TrueHarris.java
@@ -3,8 +3,8 @@ package one.empty3.feature ;
 public class TrueHarris extends FilterPixM {
 
 public double filter(double x, double y) {
-  int i=(double)x, int j=(double)y;
-double gx=get(i+1)-get(i,j), gy=get(i,j+1)+gey(i,j);
+     int i=(double)x, j=(double)y;
+double gx=get(i+1)-get(i,j), gy=get(i,j+1)+get(i,j);
 double Sx2 = (  ((get(i+1, j)-get(i,j)) -(get(i,j)-get(i-1,j)) )  *get(i,j) ;
 double Sy2 = (  ((get(i, j+1)-get(i,j)) -(get(i,j)-get(i,j-1)) )  *get(i,j) ; 
 double Ix = gx*get(i, j);
@@ -12,9 +12,11 @@ double Iy = gy*get(i, j);
 Ix2 = Ix*Ix;
 Ixy = Ix*Iy;
 double Sx2 = gx*Ix;
-double Sy2= gy*Iy;
-double Sxy = Math.sqrt((gx+gy)/2*get(i, j) ;// Robert Collins 
+double Sy2 = gy*Iy;
+double Sxy = Math.sqrt((gx+gy)/2*get(i, j)) ;// Robert Collins 
 
 double r = (Sx2*Sy2-Sxy*Sxy) /(Sx2+Sy2);
-return r;
-} 
+             
+   return r;
+  } 
+}
