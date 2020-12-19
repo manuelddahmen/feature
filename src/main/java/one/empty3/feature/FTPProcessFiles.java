fed2309c06f75ff4a3138aecbd5ac8e9cde52217
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-12-19 19:55:19 +0100

    Update FTPProcessFiles.java

--- src/main/java/one/empty3/feature/FTPProcessFiles.java
+++ src/main/java/one/empty3/feature/FTPProcessFiles.java
@@ -258,7 +258,7 @@ Class classs = Class.forName(
              FTPFile[] files1 = ftpClient.listFiles(directory);
              showServerReply(ftpClient);
                 
-                printFileDetails(file1, directory);
+                printFileDetails(files1, directory);
             } else if (server.startsWith("http")) {
                 URL oracle = new URL(server);
         BufferedReader in = new BufferedReader(
