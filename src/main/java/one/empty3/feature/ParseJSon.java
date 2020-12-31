6bf29a58388980b8c88d2460d90f2ad4a0c74cae
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-12-31 10:13:09 +0100

    Update ParseJSon.java

--- src/main/java/one/empty3/feature/ParseJSon.java
+++ src/main/java/one/empty3/feature/ParseJSon.java
@@ -12,6 +12,6 @@ JSONArray arr = obj.getJSONArray("filters");
 for (int i = 0; i < arr.length(); i++)
 {
     String filterName = arr.getJSONObject(i).getString("classname");
-    String properties = arr.gerJSONObject(i).getString("properties");
+    String properties = arr.gerJSONObject(i).getString("property");
 }
 }
