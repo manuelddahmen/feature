12029e479bdaf0d1f4b61ccdc3d0628bf3824d63
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-12-31 10:29:55 +0100

    Update ParseJSon.java

--- src/main/java/one/empty3/feature/ParseJSon.java
+++ src/main/java/one/empty3/feature/ParseJSon.java
@@ -1,17 +1,17 @@
-package one.empty3;feature;
+package one.empty3.feature;
 
 import org.json.*;
 
 
 public class ParseJSon {
-public void parse(String jsonString) {
- //assign your JSON String here
-JSONObject obj = new JSONObject(jsonString);
+   public void parse(String jsonString) {
+       //assign your JSON String here
+       JSONObject obj = new JSONObject(jsonString);
 
-JSONArray arr = obj.getJSONArray("filters");
-for (int i = 0; i < arr.length(); i++)
-{
-    String filterName = arr.getJSONObject(i).getString("classname");
-    String properties = arr.gerJSONObject(i).getString("property");
-}
+       JSONArray arr = obj.getJSONArray("filters");
+       for (int i = 0; i < arr.length(); i++) {
+           String filterName = arr.getJSONObject(i).getString("classname");
+           String properties = arr.gerJSONObject(i).getString("property");
+       }
+   }
 }
