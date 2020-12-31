a38d8831401513cae1c9e8c05c6d7c8cf7b8a543
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-12-31 09:45:56 +0100

    Update ParseJSon.java

--- src/main/java/one/empty3/feature/ParseJSon.java
+++ src/main/java/one/empty3/feature/ParseJSon.java
@@ -7,12 +7,11 @@ public class ParseJSon {
 public void parse(String jsonString) {
  //assign your JSON String here
 JSONObject obj = new JSONObject(jsonString);
-String pageName = obj.getJSONObject("pageInfo").getString("pageName");
 
-JSONArray arr = obj.getJSONArray("posts");
+JSONArray arr = obj.getJSONArray("filters");
 for (int i = 0; i < arr.length(); i++)
 {
-    String post_id = arr.getJSONObject(i).getString("post_id");
+    String filterName = arr.getJSONObject(i).getString("classname");
     //......
 }
 }
