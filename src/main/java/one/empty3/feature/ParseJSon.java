c836ffe677fae2f7f357cbe0ed040102a48f5e74
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-12-31 08:38:01 +0100

    Update ParseJSon.java

--- src/main/java/one/empty3/feature/ParseJSon.java
+++ src/main/java/one/empty3/feature/ParseJSon.java
@@ -4,8 +4,8 @@ import org.json.*;
 
 
 public class ParseJSon {
-public void parse() {/*
-String jsonString = ... ; //assign your JSON String here
+public void parse(String jsonString) {
+ //assign your JSON String here
 JSONObject obj = new JSONObject(jsonString);
 String pageName = obj.getJSONObject("pageInfo").getString("pageName");
 
@@ -13,6 +13,6 @@ JSONArray arr = obj.getJSONArray("posts");
 for (int i = 0; i < arr.length(); i++)
 {
     String post_id = arr.getJSONObject(i).getString("post_id");
-    ......*/
+    //......
 }
 }
