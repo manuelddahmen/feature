c8e13a6eba1a086a69e0a5419ee61bdb3ba26dd6
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-07 17:45:16 +0100

    Update Distance.java

--- src/main/java/one/empty3/feature/kmeans/Distance.java
+++ src/main/java/one/empty3/feature/kmeans/Distance.java
@@ -25,7 +25,14 @@ public class Distance {
         	System.exit(-1);
 		
 }
-		
+/*
+	for(int i = 0; i < 2; i++) {
+        	//System.out.println(point1[i]+" "+point2[i]);
+            sum += (
+               (point1[i] - point2[i]) * (point1[i] - point2[i])
+               )*(r[i]/10f);
+        }
+*/
         for(int i = 2; i < 5; i++) {
         	//System.out.println(point1[i]+" "+point2[i]);
             sum += ((point1[i] - point2[i]) * (point1[i] - point2[i]))*r[i]*r[i];
@@ -35,7 +42,7 @@ public class Distance {
     
     public static double manhattanDistance(double point1[], double point2[]){
     	double sum = 0.0;
-        for(int i = 2; i < 5; i++) {
+        for(int i = 0; i < 5; i++) {/*
             sum += (Math.abs(point1[i] - point2[i]))*r[i]*r[i];
         }
         return sum;
