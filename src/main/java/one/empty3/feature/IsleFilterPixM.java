dafdbd2592c32377aa333422f49255095bd05615
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-13 10:34:00 +0100

    Update IsleFilterPixM.java

--- src/main/java/one/empty3/feature/IsleFilterPixM.java
+++ src/main/java/one/empty3/feature/IsleFilterPixM.java
@@ -1,6 +1,15 @@
 package one.empty3.feature;
 
+import java.awt.Color;
+
+
 public class IsleFilterPixM 
          extends FilterPixM {
 
+
+     public void setCValues(Color background, Color sel, double threshold) {
+
+     }
+     public void filter() {
+     }
 }
