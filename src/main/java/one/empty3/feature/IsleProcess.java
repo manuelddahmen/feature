951ed497dcc0dbafb0546f7bc26895a43547453d
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-13 10:08:10 +0100

    Update IsleProcess.java

--- src/main/java/one/empty3/feature/IsleProcess.java
+++ src/main/java/one/empty3/feature/IsleProcess.java
@@ -5,6 +5,44 @@ import java.io.File;
 
 public class IsleProcess extends ProcessFile {
     public boolean process(File in, File out) {
-        return false;
+
+
+
+        if(!in.getName().endsWith(".jpg"))
+
+            return false;
+
+        File file = in;
+
+        PixM pix = null;
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(file);
+            pix = PixM.getPixM(img, 500.0);
+
+        } catch(Exception ex) {
+
+            ex.printStackTrace();
+
+            return false;
+
+       // assertTrue(false);
+
+      
+
+         }
+          
+     IsleFilterPixM th = new IsleFilterPixM
+         (pix.getImage());
+   
+      try {            
+
+          ImageIO.write (pix.getImage(), "JPEG", out) ;
+
+      } catch(Exception  ex ) {
+  
+      }
+     
+      return false;
     }
 }
