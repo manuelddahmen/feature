ce45aef8d08443a09990dd197036630029f00cfc
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-13 13:41:48 +0100

    Update IsleFilterPixM.java

--- src/main/java/one/empty3/feature/IsleFilterPixM.java
+++ src/main/java/one/empty3/feature/IsleFilterPixM.java
@@ -5,12 +5,23 @@ import java.awt.Color;
 
 public class IsleFilterPixM 
          extends FilterPixM {
+     private Color selColor;
+     private Color oppositeColor;
+     private double threshold:
+     private float selComps[];
+     private float oppComps[];
      public IsleFilterPixM(PixM pix) {
          super(pix);
      }
-
+     public boolean selectPoint(int x, inty) {
+     }
      public void setCValues(Color background, 
          Color sel, double threshold) {
+       this.oppositeColor = background;
+       this.selColor = sel;
+       this.threshold = threshold;
+       this.selComps ;
+       this.oppComps ;
 
      }
      public void filter() {
