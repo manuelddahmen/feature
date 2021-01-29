8c137d52b3380f01277bcde587e756fc2945e724
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-29 13:48:07 +0100

    Create DBScan.java

---
+++ src/main/java/one/empty3/feature/DBScan.java
@@ -0,0 +1,25 @@
+/*
+DBSCAN(D, eps, MinPts)
+   C = 0
+   pour chaque point P non visité des données D
+      marquer P comme visité
+      PtsVoisins = epsilonVoisinage(D, P, eps)
+      si tailleDe(PtsVoisins) &lt; MinPts
+         marquer P comme BRUIT
+      sinon
+         C++
+         etendreCluster(D, P, PtsVoisins, C, eps, MinPts)
+          
+etendreCluster(D, P, PtsVoisins, C, eps, MinPts)
+   ajouter P au cluster C
+   pour chaque point P' de PtsVoisins 
+      si P' n'a pas été visité
+         marquer P' comme visité
+         PtsVoisins' = epsilonVoisinage(D, P', eps)
+         si tailleDe(PtsVoisins') >= MinPts
+            PtsVoisins = PtsVoisins U PtsVoisins'
+      si P' n'est membre d'aucun cluster
+         ajouter P' au cluster C
+          
+epsilonVoisinage(D, P, eps)
+ */
