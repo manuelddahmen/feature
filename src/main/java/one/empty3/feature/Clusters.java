e57c349a84988f317c0e04aa808201c7d68f5293
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-30 09:05:10 +0100

    Update Clusters.java

--- src/main/java/one/empty3/feature/Clusters.java
+++ src/main/java/one/empty3/feature/Clusters.java
@@ -32,6 +32,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
+import java.io.File;
 public class Clusters extends ProcessFile {
     public boolean process (File in, File out) {
         return false;
