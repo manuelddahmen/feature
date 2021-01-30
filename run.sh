0b940fb51f473c2c502ebd5bc5cdc6d3a63114fb
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-01-30 10:49:01 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-for i in sets/$1
-do
-    echo "${1}"
-    mvn exec:java -Dexec.mainClass="one.empty3.feature.FTPProcessFiles" -Dexec.args="sets/$1"
-done
+mvn exec:java -Dexec.mainClass="one.empty3.feature.FTPProcessFiles" -Dexec.args="sets/$1"
+
