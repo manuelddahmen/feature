a282492658910cbb3f28ae958b5a41bc8e35a968
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-02-13 14:35:44 +0100

    Update DBScanProcess.java

--- src/main/java/one/empty3/feature/DBScanProcess.java
+++ src/main/java/one/empty3/feature/DBScanProcess.java
@@ -53,40 +53,6 @@ for (double[] q : N) {
            }
 
      }
-DBSCAN(DB, distFunc, eps, minPts) {
-    C := 0                                                  /* Cluster counter */
-    for each point P in database DB {
-        if label(P) ≠ undefined then continue               /* Previously processed in inner loop */
-        Neighbors N := RangeQuery(DB, distFunc, P, eps)     /* Find neighbors */
-        if |N| < minPts then {                              /* Density check */
-            label(P) := Noise                               /* Label as Noise */
-            continue
-        }
-        C := C + 1                                          /* next cluster label */
-        label(P) := C                                       /* Label initial point */
-        SeedSet S := N \ {P}                                /* Neighbors to expand */
-        for each point Q in S {                             /* Process every seed point Q */
-            if label(Q) = Noise then label(Q) := C          /* Change Noise to border point */
-            if label(Q) ≠ undefined then continue           /* Previously processed (e.g., border point) */
-            label(Q) := C                                   /* Label neighbor */
-            Neighbors N := RangeQuery(DB, distFunc, Q, eps) /* Find neighbors */
-            if |N| ≥ minPts then {                          /* Density check (if Q is a core point) */
-                S := S ∪ N                                  /* Add new neighbors to seed set */
-            }
-        }
-    }
-}
-where RangeQuery can be implemented using a database index for better performance, or using a slow linear scan:
-
-RangeQuery(DB, distFunc, Q, eps) {
-    Neighbors N := empty list
-    for each point P in database DB {                      /* Scan all points in the database */
-        if distFunc(Q, P) ≤ eps then {                     /* Compute distance and check epsilon */
-            N := N ∪ {P}                                   /* Add to result */
-        }
-    }
-    return N
-}
      public double distance(double [] p1,
           double [] p2) {
              double d =0.0;
