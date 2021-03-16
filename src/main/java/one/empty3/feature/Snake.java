0d5b5b4407143f50b9dd5526d104dcda68cfd2a2
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-16 14:11:55 +0100

    Update Snake.java

--- src/main/java/one/empty3/feature/Snake.java
+++ src/main/java/one/empty3/feature/Snake.java
@@ -1,7 +1,25 @@
 package one.empty3.feature;
+
+
 import java.io.*;
 import java.util.*;
+import one.empty3.io.ProcessFile;
 
 public class Snake {
 
+    private BSpline spline;
+
+
+    private double energy() {
+        double e;
+        return e;
+    }
+
+    public boolean process(File in, File out) {
+        try {
+            PixM pix = new PixM(ImageIO.read(in));
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
 }
