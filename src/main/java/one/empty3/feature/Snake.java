3469bc65d99162228e5fd22179b222cca687fe95
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2021-03-16 17:18:07 +0100

    Update Snake.java

--- src/main/java/one/empty3/feature/Snake.java
+++ src/main/java/one/empty3/feature/Snake.java
@@ -5,7 +5,7 @@ import java.io.*;
 import java.util.*;
 import one.empty3.io.ProcessFile;
 import one.empty3.library.*;
-import javax.image.ImageIO;
+import javax.imageio.ImageIO;
 /***
 1 prendre les points entre t=0.0 et t=1.0
   extraire un polygone interieur et exterieur,
