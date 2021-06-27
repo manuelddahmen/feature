9e4a47cac4c912b3dd5ee29c828f21e76352c744
Manuel DAHMEN <manuel.dahmen@gmx.com>
2021-06-27 10:31:31 +0200

    Tentative imitation Viola-Jones.

--- encode.bat
+++ encode.bat
@@ -1,14 +1,3 @@
-set PATH_TO_FFMPEG=c:\users\manue\ffmpeg\bin
-cd ffmpeg\data
-cd manu
-..\..\bin\ffmpeg.exe -f image2 -vcodec libx264 -b 4M -vpre normal -acodec copy -s 1280x720 -i manu-%%03d.jpg ..\..\video-manu.mpg
-cd ..
-cd manu-dotted
-..\..\bin\ffmpeg.exe -f image2 -vcodec libx264 -b 4M -vpre normal -acodec copy -s 1280x720 -i manu-%%03d.jpg-dotted.jpg ..\..\video-manu-dotted.mpg
-cd ..
-cd manu2
-..\..\bin\ffmpeg.exe -f image2 -vcodec libx264 -b 4M -vpre normal -acodec copy -s 1280x720 -i foo2-%%03d.jpg ..\..\video-manu2.mpg
-cd ..
-cd foof
-..\..\bin\ffmpeg.exe -f image2 -vcodec libx264 -b 4M -vpre normal -acodec copy -s 1280x720 -i foo-%%03d.jpg ..\..\video-manu2.mpg
-cd ..
+set PATH_TO_FFMPEG=c:\users\manue\ideaProjects\feature\ffmpeg\bin
+cd o\allpass-noharris-2-one.empty3.feature.Lines4
+%PATH_TO_FFMPEG%\ffmpeg.exe -f image2 -s 1280x720 -i foo2-%%3d.jpg video-manu2.mp4
